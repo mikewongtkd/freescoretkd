8f754a02f2ed07167823036605c91cc5a491d56c
mikewong899 <mikewong899@6256fea2-052d-4951-84f0-b84678e92dfd>
2013-07-25 13:55:28 +0000

    Added event handlers for improved UI interaction
    git-svn-id: file:///svn/freescore@7 6256fea2-052d-4951-84f0-b84678e92dfd

--- trunk/frontend/html/include/js/jquery.staging.js
+++ trunk/frontend/html/include/js/jquery.staging.js
@@ -7,8 +7,9 @@
 			var div     = $( "<div />" );
 			var divinfo = this.options.division;
 			divinfo.description = divinfo.gender + " " + divinfo.age + " " + divinfo.rank;
-			var division    = div.clone() .addClass( 'division' );
-			var header      = div.clone() .addClass( 'header' );
+			var division    = div.clone() .addClass( 'division' ) .addClass( 'subscribers-selection' ) .disableSelection();
+			var header      = div.clone() .addClass( 'header' ) .disableSelection();
+			var id          = div.clone() .addClass( 'id' ) .append( divinfo.id );
 			var description = div.clone() .addClass( 'description' ) .append( divinfo.description );
 			var message     = div.clone() .addClass( 'message' );
 			var weight      = div.clone() .addClass( 'weight' ) .append( divinfo.weight );
@@ -18,78 +19,80 @@
 			this.header   = header;
 			this.message  = message;
 
+			header   .append( id );
 			header   .append( description );
 			header   .append( message );
 			header   .append( weight );
 			division .append( header );
 			division .append( athletes );
 
+			// ===== 
+			var icon;
+			var text;
 			if(        divinfo.athletes.length == 1 ) {
-				var icon = div .clone() .addClass( 'ui-icon' ) .addClass( 'ui-icon-custom-error' ) .addClass( 'ui-icon-alert' );
-				var text = div .clone() .append( ' <b>Alert!</b> Not enough athletes' );
-				message
-					.append( icon )
-					.append( text );
-				division .addClass( 'error' );
-				header   .addClass( 'error' );
+				icon = div .clone() .addClass( 'ui-icon' ) .addClass( 'ui-icon-alert' );
+				text = div .clone() .append( ' <b>Alert!</b> Not enough athletes' );
+				division .addClass( 'state-error' );
 
 			} else if( divinfo.exhibition ) {
-				var icon = div .clone() .addClass( 'ui-icon' ) .addClass( 'ui-icon-custom-warning' ) .addClass( 'ui-icon-check' );
-				var text = div .clone() .append( ' Ready to assign to a ring as an <b>Exhibition Match</b>' );
-				message
-					.append( icon )
-					.append( text );
-				division .addClass( 'warning' );
-				header   .addClass( 'warning' );
+				icon = div .clone() .addClass( 'ui-icon' ) .addClass( 'ui-icon-check' );
+				text = div .clone() .append( ' Ready to assign to a ring as an <b>Exhibition Match</b>' );
+				division .addClass( 'state-warning' );
 			} else {
-				var icon = div .clone() .addClass( 'ui-icon' ) .addClass( 'ui-icon-custom-ok' ) .addClass( 'ui-icon-check' );
-				var text = div .clone() .append( ' Ready to assign to a ring as a <b>Regulation Division</b>' );
-				message
-					.append( icon )
-					.append( text );
-				division .addClass( 'ok' );
-				header   .addClass( 'ok' );
+				icon = div .clone() .addClass( 'ui-icon' ) .addClass( 'ui-icon-check' );
+				text = div .clone() .append( ' Ready to assign to a ring as a <b>Regulation Division</b>' );
+				division .addClass( 'state-ok' );
 			}
-			for (var j = 0; j < divinfo.athletes.length; j++ ) {
-				var athlete_info = divinfo.athletes[ j ];
+			message .append( icon ) .append( text );
+
+			// ===== HANDLE INTERACTIVITY BEHAVIOR
+			division.hover(
+				function() {
+					if     ( $( this ).hasClass( 'state-error' ))   { $( this ).addClass( 'state-error-selecting' );       }
+					else if( $( this ).hasClass( 'state-warning' )) { $( this ).addClass( 'state-warning-selecting' );     } 
+					else if( $( this ).hasClass( 'state-ok' ))      { $( this ).addClass( 'state-ok-selecting' );          }
+					else                                            { $( this ).addClass( 'state-selected-selecting' );    }
+				},
+				function() {
+					if     ( $( this ).hasClass( 'state-error' ))   { $( this ).removeClass( 'state-error-selecting' );    }
+					else if( $( this ).hasClass( 'state-warning' )) { $( this ).removeClass( 'state-warning-selecting' );  }
+					else if( $( this ).hasClass( 'state-ok' ))      { $( this ).removeClass( 'state-ok-selecting' );       }
+					else                                            { $( this ).removeClass( 'state-selected-selecting' ); }
+				}
+			);
+
+			division.click( function() {
+				$( this ).toggleClass( 'state-selected' );
+				var id         = $( this ).children( '.header' ).children( '.id' ).text();
+				var isSelected = $( this ).hasClass( 'state-selected' );
+				$( '.subscribers-selection' ).trigger( 'selected', [ id, isSelected ] );
+				$( '.staging' ).trigger( 'selected', [ id, isSelected ] );
+			});
+
+			division.on( 'selected', function( event, sender, isSelected ) {
+				var id = $( this ).children( '.header' ).children( '.id' ).text();
+				
+				if( sender == id ) {
+				} else {
+					$( this ).removeClass( 'state-selected' );
+				}
+				return false;
+			});
+
+			// ===== ASSIGN ATHLETE DATA
+			for (var i = 0; i < divinfo.athletes.length; i++ ) {
+				var athleteInfo = divinfo.athletes[ i ];
 				var athlete = div.clone() .addClass( 'athlete' );
 				athletes
 					.append( 
 						athlete
-							.append( div.clone() .addClass( 'id' )         .append( athlete_info.id ))
-							.append( div.clone() .addClass( 'name' )       .append( athlete_info.fname + ' ' + athlete_info.lname ) )
-							.append( div.clone() .addClass( 'age-weight' ) .append( athlete_info.belt + ', ' + athlete_info.age + 'yo, ' + athlete_info.weight + ' lbs.' ))
+							.append( div.clone() .addClass( 'id' )         .append( athleteInfo.id ))
+							.append( div.clone() .addClass( 'name' )       .append( athleteInfo.fname + ' ' + athleteInfo.lname ) )
+							.append( div.clone() .addClass( 'age-weight' ) .append( athleteInfo.belt + ', ' + athleteInfo.age + 'yo, ' + athleteInfo.weight + ' lbs.' ))
 					);
 			}
 			this.element .append( division );
 		},
-
-		handleInHover : function() {
-			if       ( $( this ).hasClass( 'error' ) ) {
-				this.division.addClass( 'selecting-error' );
-				this.header.addClass( 'selecting-header-error' );
-
-			} else if( $( this ).hasClass( 'warning' )) {
-				this.division.addClass( 'selecting-warning' );
-
-			} else {
-				this.division.addClass( 'selecting-ok' );
-			}
-		},
-
-		handleOutHover : function() {
-			if       ( $( this ).hasClass( 'error' ) ) {
-				this.division.removeClass( 'selecting-error' );
-				this.header.removeClass( 'selecting-header-error' );
-
-			} else if( $( this ).hasClass( 'warning' )) {
-				this.division.removeClass( 'selecting-warning' );
-
-			} else {
-				this.division.removeClass( 'selecting-ok' );
-			}
-		}
-
 	});
 
 	$.widget( "freescore.staging", {
@@ -98,44 +101,73 @@
 		},
 		_create: function() {
 			var div = $( "<div />" );
-			var header             = div.clone() .addClass( 'staging-header' );
-			var tournament_title   = $( "<h1 />" );
-			var event_name         = $( "<h2 />" );
-			var staging            = div.clone() .addClass( 'staging' );
-			var divisions          = div.clone() .addClass( 'divisions' );
-			var buttons            = div.clone() .addClass( 'buttons' );
-			var message            = div.clone() .addClass( 'user-message' );
-			var split_button       = div.clone() .addClass( 'split' )  .addClass( 'button' ) .append( 'Split' );
-			var merge_button       = div.clone() .addClass( 'merge' )  .addClass( 'button' ) .append( 'Merge' );
-			var assign_button      = div.clone() .addClass( 'assign' ) .addClass( 'button' ) .append( 'Assign to a Ring' );
+			var header          = div.clone() .addClass( 'staging-header' );
+			var tournamentTitle = $( "<h1 />" );
+			var eventName       = $( "<h2 />" );
+			var staging         = div.clone() .addClass( 'staging' );
+			var divisions       = div.clone() .addClass( 'divisions' );
+			var buttons         = div.clone() .addClass( 'buttons' );
+			var message         = div.clone() .addClass( 'user-message' );
+			var splitButton     = div.clone() .addClass( 'split' )  .addClass( 'button' ) .append( 'Split' );
+			var mergeButton     = div.clone() .addClass( 'merge' )  .addClass( 'button' ) .append( 'Merge' );
+			var assignButton    = div.clone() .addClass( 'assign' ) .addClass( 'button' ) .append( 'Assign to a Ring' );
 
-			header.append( tournament_title );
-			header.append( event_name );
+			header.append( tournamentTitle );
+			header.append( eventName );
 			this.element.append( header );
 			this.element.append( staging );
 
-			staging .append( buttons )   .addClass( 'buttons' );
-			staging .append( divisions ) .addClass( 'divisions' );
-			staging .append( message )   .addClass( 'user-message' );
+			staging .append( buttons );
+			staging .append( divisions );
+			staging .append( message );
 
-			buttons .append( split_button, merge_button, assign_button );
+			buttons .append( splitButton, mergeButton, assignButton );
 			message .append( 'Click on a division to split the division, merge the division with another, or assign the division to a ring' );
 
 			var url = {
 				'tournament' : $.url().param( 'tournament' ),
 				'event'      : $.url().param( 'event' )
 			};
+
 			$.getJSON( 
 				'https://localhost/cgi-bin/freescore/rest/' + url.tournament + '/divisions/' + url.event,
 				function( tournament ) {
-					tournament_title.append( tournament.name );
-					event_name .append( tournament.event );
+					tournamentTitle.append( tournament.name );
+					eventName .append( tournament.event );
 					for ( var i = 0; i < tournament.divisions.length; i++ ) {
 						var division = tournament.divisions[ i ];
 						divisions.stagingDivision( { 'division' : division } );
 					}
 				}
 			);
+
+			var buttonArray = [ splitButton, mergeButton, assignButton ];
+			for (var i in buttonArray ) {
+				var button = buttonArray[ i ]
+				button.hover(
+					function() {
+						if( ! $( this ).hasClass( 'state-enabled' )) { return false; }
+						$( this ).addClass( 'state-hover' );
+						
+					},
+					function() {
+						if( ! $( this ).hasClass( 'state-enabled' )) { return false; }
+						$( this ).removeClass( 'state-hover' );
+					}
+				);
+			}
+
+			staging.on( 'selected', function( event, sender, isSelected ) {
+				if( isSelected ) {
+					$( '.button' ).addClass( 'state-enabled' );
+					$( '.user-message' ).html( 'Click on a button above to split, merge, or assign division #' + sender + ' to a ring.' );
+				} else {
+					$( '.button' ).removeClass( 'state-enabled' );
+					$( '.user-message' ).html( 'Click on a division to split the division, merge the division with another, or assign the division to a ring' );
+				}
+				return false;
+			});
+
 		},
 
 		_init: function() {
