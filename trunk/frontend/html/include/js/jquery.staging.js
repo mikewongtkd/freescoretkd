58a2e038ebe569e2c35277340cf6cb8ad4a48f66
mikewong899 <mikewong899@6256fea2-052d-4951-84f0-b84678e92dfd>
2013-07-26 17:29:24 +0000

    Adding merging widget and hexagonal divison visualization.
    git-svn-id: file:///svn/freescore@8 6256fea2-052d-4951-84f0-b84678e92dfd

--- trunk/frontend/html/include/js/jquery.staging.js
+++ trunk/frontend/html/include/js/jquery.staging.js
@@ -1,5 +1,60 @@
 (function( $ ) {
-	$.widget( "freescore.stagingDivision", {
+	$.widget( "freescore.splitDialog", {
+		options: {
+			autoShow: true,
+		},
+		_create: function() {
+			var divisions = this.options.divisions;
+		}
+	});
+
+	$.widget( "freescore.mergeDialog", {
+		options: {
+			autoShow: true,
+		},
+		_create: function() {
+			var div       = $( "<div />" );
+			var canvas    = $( "<canvas />" ) .attr( 'width', this.options.width) .attr( 'height', this.options.height) .addClass( 'division-visualization' );
+
+			this.element.append( canvas );
+			var hexagon = function( canvas, x, y, z, size, color ) {
+				var origin  = { 'x': (canvas.width() / 2),  'y': (canvas.height() / 2) };
+
+				var points = [];
+				for (var i = 1; i <= 6; i += 1) {
+					points.push( [ origin.x + size * Math.cos(i * 2 * Math.PI / 6), origin.y + size * Math.sin(i * 2 * Math.PI / 6) ]);
+				}
+
+				canvas.drawLine( {
+					closed: true,
+					fillStyle: color.fill,
+					strokeStyle: color.stroke,
+					strokeWidth: 1,	
+					x1: points[ 0 ][ 0 ], y1: points[ 0 ][ 1 ],
+					x2: points[ 1 ][ 0 ], y2: points[ 1 ][ 1 ],
+					x3: points[ 2 ][ 0 ], y3: points[ 2 ][ 1 ],
+					x4: points[ 3 ][ 0 ], y4: points[ 3 ][ 1 ],
+					x5: points[ 4 ][ 0 ], y5: points[ 4 ][ 1 ],
+					x6: points[ 5 ][ 0 ], y6: points[ 5 ][ 1 ],
+				});
+			};
+			// var positions = [[ -1, 0, 0 ], [ 0, -1, 0 ], [ 0, 0, -1 ], [ 1, 0, 0 ], [ 0, 1, 0 ], [ 0, 0, 1 ]];
+			var positions = [[ 0, 0, 0 ]];
+			for( var i in positions ) {
+				var x     = positions[ i ][ 0 ];
+				var y     = positions[ i ][ 1 ];
+				var z     = positions[ i ][ 2 ];
+				var color = { 'stroke' : '#090', 'fill' : '#9f9' };
+				hexagon( canvas, x, y, z, 64, color );
+			}
+		},
+		_init: function() {
+			var divisions = this.options.divisions;
+			var selection = this.options.selection;
+		}
+	});
+
+	$.widget( "freescore.stage", {
 		options: {
 			autoShow: true,
 		},
@@ -63,16 +118,15 @@
 
 			division.click( function() {
 				$( this ).toggleClass( 'state-selected' );
-				var id         = $( this ).children( '.header' ).children( '.id' ).text();
 				var isSelected = $( this ).hasClass( 'state-selected' );
-				$( '.subscribers-selection' ).trigger( 'selected', [ id, isSelected ] );
-				$( '.staging' ).trigger( 'selected', [ id, isSelected ] );
+				$( '.subscribers-selection' ).trigger( 'selected', [ divinfo, isSelected ] );
+				$( '.staging' ).trigger( 'selected', [ divinfo, isSelected ] );
 			});
 
 			division.on( 'selected', function( event, sender, isSelected ) {
 				var id = $( this ).children( '.header' ).children( '.id' ).text();
 				
-				if( sender == id ) {
+				if( sender.id == id ) {
 				} else {
 					$( this ).removeClass( 'state-selected' );
 				}
@@ -105,12 +159,19 @@
 			var tournamentTitle = $( "<h1 />" );
 			var eventName       = $( "<h2 />" );
 			var staging         = div.clone() .addClass( 'staging' );
+			var mergeDialog     = div.clone() .addClass( 'dialog' ) .addClass( 'merge-dialog' );
+			var splitDialog     = div.clone() .addClass( 'dialog' ) .addClass( 'split-dialog' );
+			var assignDialog    = div.clone() .addClass( 'dialog' ) .addClass( 'assign-dialog' );
 			var divisions       = div.clone() .addClass( 'divisions' );
 			var buttons         = div.clone() .addClass( 'buttons' );
 			var message         = div.clone() .addClass( 'user-message' );
 			var splitButton     = div.clone() .addClass( 'split' )  .addClass( 'button' ) .append( 'Split' );
 			var mergeButton     = div.clone() .addClass( 'merge' )  .addClass( 'button' ) .append( 'Merge' );
 			var assignButton    = div.clone() .addClass( 'assign' ) .addClass( 'button' ) .append( 'Assign to a Ring' );
+			var tournament      = undefined;
+			var selection       = undefined;
+			var splitWidget     = undefined;
+			var mergeWidget     = undefined;
 
 			header.append( tournamentTitle );
 			header.append( eventName );
@@ -119,6 +180,9 @@
 
 			staging .append( buttons );
 			staging .append( divisions );
+			staging .append( mergeDialog );
+			staging .append( splitDialog );
+			staging .append( assignDialog );
 			staging .append( message );
 
 			buttons .append( splitButton, mergeButton, assignButton );
@@ -131,12 +195,13 @@
 
 			$.getJSON( 
 				'https://localhost/cgi-bin/freescore/rest/' + url.tournament + '/divisions/' + url.event,
-				function( tournament ) {
+				function( data ) {
+					tournament = data;
 					tournamentTitle.append( tournament.name );
 					eventName .append( tournament.event );
 					for ( var i = 0; i < tournament.divisions.length; i++ ) {
 						var division = tournament.divisions[ i ];
-						divisions.stagingDivision( { 'division' : division } );
+						divisions.stage( { 'division' : division } );
 					}
 				}
 			);
@@ -156,11 +221,43 @@
 					}
 				);
 			}
+			var width  = staging.width();
+			var height = staging.height();
+			splitButton.click(
+				function() {
+					var dialog = $( '.split-dialog' );
+					if( ! $( this ).hasClass( 'state-enabled' )) { return false; }
+					if( dialog.hasClass( 'state-show-dialog' )) {
+						dialog.removeClass( 'state-show-dialog' );
+					} else {
+						dialog.addClass( 'state-show-dialog' );
+						splitWidget = dialog.splitDialog( { 'width': width, 'height': height, 'divisions' : tournament.divisions, 'selection' : selection  } );
+					}
+				}
+			);
+			mergeButton.click(
+				function() {
+					var dialog = $( '.merge-dialog' );
+					if( ! $( this ).hasClass( 'state-enabled' )) { return false; }
+					if( dialog.hasClass( 'state-show-dialog' )) {
+						dialog.removeClass( 'state-show-dialog' );
+					} else {
+						dialog.addClass( 'state-show-dialog' );
+						mergeWidget = dialog.mergeDialog( { 'width': width, 'height': height, 'divisions' : tournament.divisions, 'selection' : selection  } );
+					}
+				}
+			);
 
 			staging.on( 'selected', function( event, sender, isSelected ) {
 				if( isSelected ) {
-					$( '.button' ).addClass( 'state-enabled' );
-					$( '.user-message' ).html( 'Click on a button above to split, merge, or assign division #' + sender + ' to a ring.' );
+					selection = sender;
+					if( selection.athletes.length > 1 ) {
+						$( '.button' ).addClass( 'state-enabled' );
+						$( '.user-message' ).html( 'Click on a button above to split, merge, or assign division #' + selection.id + ' <b>' + selection.description + ', ' + selection.weight + '</b> to a ring.' );
+					} else {
+						$( '.merge' ).addClass( 'state-enabled' );
+						$( '.user-message' ).html( 'Click on a button above to merge division #' + selection.id + ' <b>' + selection.description + ', ' + selection.weight + '</b> with another division.' );
+					}
 				} else {
 					$( '.button' ).removeClass( 'state-enabled' );
 					$( '.user-message' ).html( 'Click on a division to split the division, merge the division with another, or assign the division to a ring' );
