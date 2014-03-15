8f8e8f1c9f91475bd0c5aa8f3d083368c7040c65
mikewong <mikewong@6256fea2-052d-4951-84f0-b84678e92dfd>
2014-03-15 03:36:09 +0000

    Found some CSS voodoo using fixed positions with relative distances.
    git-svn-id: file:///svn/freescore@24 6256fea2-052d-4951-84f0-b84678e92dfd

--- trunk/frontend/html/include/js/forms/grassroots/jquery.leaderBoard.js
+++ trunk/frontend/html/include/js/forms/grassroots/jquery.leaderBoard.js
@@ -1,14 +1,14 @@
 $.widget( "freescore.leaderboard", {
 	options: { autoShow: true, twoThirds: true },
 	_create: function() {
-		var options    = this.options;
-		var e          = this.options.elements = {};
-		var html       = { div : $( "<div />" ) };
-		var division   = e.division   = html.div.clone() .addClass( "division" );
-		var pending    = e.pending    = html.div.clone() .addClass( "pending" );
-		var placements = e.placements = html.div.clone() .addClass( "placements" );
+		var options   = this.options;
+		var e         = this.options.elements = {};
+		var html      = { div : $( "<div />" ) };
+		var division  = e.division  = html.div.clone() .addClass( "division" );
+		var pending   = e.pending   = html.div.clone() .addClass( "pending" );
+		var standings = e.standings = html.div.clone() .addClass( "standings" );
 
-		division .append( pending, placements );
+		division .append( pending, standings );
 
 		this.element .addClass( "leaderboard" );
 		this.element .append( division );
@@ -20,7 +20,7 @@ $.widget( "freescore.leaderboard", {
 		var widget     = this.element;
 		var athletes   = this.options.division.athletes;
 		var pending    = { list: html.ol.clone(), athletes: new Array() };
-		var placements = { list: html.ol.clone(), athletes: new Array() };
+		var standings = { athletes: new Array() };
 		var url        = {
 			tournament : $.url().param( 'tournament' ),
 			division   : $.url().param( 'division' ),
@@ -36,20 +36,18 @@ $.widget( "freescore.leaderboard", {
 				if( score > 0.0 ) { sum += score; }
 			}
 			athlete.score = sum;
-			if( done ) { placements.athletes.push( athlete ); }
+			if( done ) { standings.athletes.push( athlete ); }
 			else       { pending.athletes.push( athlete ); }
 		}
 
-		placements.athletes.sort( function( a, b ) { return parseFloat( b.score ) - parseFloat( a.score ); } );
-		e.placements.empty();
-		e.placements.append( "<h2>Placements</h2>" );
-		e.placements.append( placements.list );
-		var k = placements.athletes.length < 4 ? placements.athletes.length : 4;
+		standings.athletes.sort( function( a, b ) { return parseFloat( b.score ) - parseFloat( a.score ); } );
+		e.standings.empty();
+		e.standings.append( "<h2>Current Standings</h2>" );
+		var k = standings.athletes.length < 4 ? standings.athletes.length : 4;
 		for( var i = 0; i < k; i++ ) {
 			var item = html.li.clone();
-			var athlete = placements.athletes[ i ];
-			item.append(  athlete.name + " (" + athlete.score.toFixed( 1 ) + ")" );
-			placements.list.append( item );
+			var athlete = standings.athletes[ i ];
+			e.standings.append(  "<div class=\"athlete\"><div class=\"name rank" + (i + 1) + "\">" + athlete.name + "</div><div class=\"score\">" + athlete.score.toFixed( 1 ) + "</div><div class=\"medal\"><img src=\"/freescore/images/medals/rank" + (i + 1) + ".png\" align=\"right\" /></div></div>" );
 		}
 		
 		e.pending.empty();
