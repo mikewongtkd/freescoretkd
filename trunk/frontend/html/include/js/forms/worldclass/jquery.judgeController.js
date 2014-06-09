3646e7ada59b72095ad888e9d413cd70d4c3f858
mikewong <mikewong@6256fea2-052d-4951-84f0-b84678e92dfd>
2014-06-09 04:36:51 +0000

    Adding widget to add navigation to the world class judge controller.
    git-svn-id: file:///svn/freescore@45 6256fea2-052d-4951-84f0-b84678e92dfd

--- trunk/frontend/html/include/js/forms/worldclass/jquery.judgeController.js
+++ trunk/frontend/html/include/js/forms/worldclass/jquery.judgeController.js
@@ -12,17 +12,23 @@ $.widget( "freescore.judgeController", {
 		login.dialog( { 
 			autoOpen: true, resizeable: false, modal: true, width: '400px', 
 			buttons: {
-				'Judge 1'    : function() { o.num = 0; $( this ).dialog( "close" ); },
-				'Center Ref' : function() { o.num = 1; $( this ).dialog( "close" ); },
-				'Judge 3'    : function() { o.num = 2; $( this ).dialog( "close" ); },
+				'Judge 1'  : function() { o.num =  0; $( this ).dialog( "close" ); },
+				'Judge 2'  : function() { o.num =  1; $( this ).dialog( "close" ); },
+				'Judge 3'  : function() { o.num =  2; $( this ).dialog( "close" ); },
+				'Judge 4'  : function() { o.num =  3; $( this ).dialog( "close" ); },
+				'Judge 5'  : function() { o.num =  4; $( this ).dialog( "close" ); },
+				'Operator' : function() { o.num = -1; $( this ).dialog( "close" ); },
 			}
 		} );
 
 		this.element.addClass( 'judgeController' );
 
-		var views        = e.views        = html.div.clone() .addClass( "control-group" );
-		var controllers  = e.controllers  = html.div.clone() .addClass( "control-group" );
+		var front        = e.front        = html.div.clone() .addClass( "front" );
+		var views        = e.views        = html.div.clone() .addClass( "view control-group" );
+		var controllers  = e.controllers  = html.div.clone() .addClass( "controller control-group" );
 
+		var flipToBack   = e.fliptoBlack  = html.div.clone() .addClass( "flip" ) .html( "Division" );
+		var athlete      = e.athlete      = html.div.clone() .addClass( "athlete" );
 		var score        = e.score        = html.div.clone() .addClass( "score" );
 		var accuracy     = e.accuracy     = html.div.clone() .addClass( "accuracy" );
 		var presentation = e.presentation = html.div.clone() .addClass( "presentation" );
@@ -38,15 +44,24 @@ $.widget( "freescore.judgeController", {
 		var ki           = e.ki           = html.div.clone() .presentationBar({ label : 'Expression of Ki' });
 		var send         = e.send         = html.div.clone() .ajaxbutton({ server : o.server, tournament : o.tournament, command : "forms/worldclass", label : "Send", type : "send" })
 
-		score.append( accuracy, presentation );
-		views.append( score, matPosition );
+		score.append( accuracy, presentation, athlete );
+		views.append( flipToBack, score, matPosition );
 		controls.append( label, rhythm, power, ki, send );
 
 		controllers.append( major, controls, minor );
 
+		front.append( views, controllers );
 		var notes        = e.notes        = html.div.clone() .judgeNotes({ athletes : [], current : 0 });
 
-		this.element.append( views, controllers );
+		widget.append( front );
+
+		// ============================================================
+		// BEHAVIOR
+		// ============================================================
+		flipToBack.click( function() {
+			flipToBack.fadeTo( 50, 0.75, function() { flipToBack.fadeTo( 100, 1.0 ); } );
+			// Do flippy thing
+		});
 
 	},
 	_init: function( ) {
@@ -57,7 +72,7 @@ $.widget( "freescore.judgeController", {
 
 		function refresh( update ) {
 			var division = JSON.parse( update.data );
-			var command      = function( judge, score ) {
+			var command  = function( judge, score ) {
 
 				score.accuracy     = 4.0 -
 				                     e.major  .deductions( 'option', 'count' ) * e.major .deductions( 'option', 'value' ) -
@@ -74,6 +89,7 @@ $.widget( "freescore.judgeController", {
 			}
 			e.send .ajaxbutton({ command : command( o.num, o ) });
 			e.notes .judgeNotes({ athletes : division.athletes, current : division.current });
+			e.athlete .html( "Athlete Name" );
 
 		};
 		e.source = new EventSource( 'update.php' );
