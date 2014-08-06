8d4d9ae462c545f0ab57f171f0d515a07f78fc2b
mikewong <mikewong@6256fea2-052d-4951-84f0-b84678e92dfd>
2014-08-06 04:46:12 +0000

    Further updates to support ring assignment.
    git-svn-id: file:///svn/freescore@63 6256fea2-052d-4951-84f0-b84678e92dfd

--- trunk/frontend/html/include/js/setup/jquery.ring.js
+++ trunk/frontend/html/include/js/setup/jquery.ring.js
@@ -4,11 +4,22 @@ $.widget( "freescore.ring", {
 		var o        = this.options;
 		var e        = this.options.elements = {};
 		var w        = this.element;
-		var html     = o.html     = { div : $( "<div />" ), h1 : $( "<h1 />" ), p : $( "<p />" ) };
+		var html     = o.html     = { div : $( "<div />" ), h1 : $( "<h1 />" ), p : $( "<p />" ), form : $( "<form />" ), select : $( "<select />" ), option : $( "<option />" ), submit : $( "<input type='button' />" ) };
 
-		w.append( html.h1.clone().html( "Register This Device" ));
-		w.append( html.p.clone().html( "Ring: <form><select><option value=1>1</option></select></form>" ));
+		var h1     = html.h1.clone() .html( "Register This Device" );
+		var text   = html.p.clone() .html( "Ring: " );
+		var form   = html.form.clone();
+		var select = html.select.clone();
+
+		for( var i = 1; i <= o.tournament.rings; i++ ) {
+			var option = html.option.clone() .prop( "value", i ) .html( i );
+			select.append( option );
+		}
+		var submit = html.submit.clone() .prop( "value", "Register" );
+		form.append( select, submit );
+		text.append( form );
 		
+		w.append( h1, text );
 		w.addClass( "ring" );
 	},
 	_init: function( ) {
--- trunk/frontend/html/include/php/config.php
+++ trunk/frontend/html/include/php/config.php
@@ -1,9 +1,11 @@
 <?php
 	$host = "169.254.128.39";
-	$host = "localhost";
+	// $host = "localhost";
 	// $host = "192.168.1.111";
 	$tournament = json_encode( [ 
+		"name" => "Tiger Martial Arts Spring Tournament 2014",
 		"db" => "tma-2014-spring", 
+		"rings" => 6,
 		"forms" => [ 
 			"worldclass" => [ "judges" => 5 ], 
 			"grassroots" => [ "judges" => 3 ]
