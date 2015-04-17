8e0865815e064678f4284666bfa601254f7685e5
Mike Wong <mikewong@sfsu.edu>
2015-04-17 16:53:56 -0700

    Adding jQuery mobile.

--- trunk/frontend/html/include/php/config.php
+++ trunk/frontend/html/include/php/config.php
@@ -2,7 +2,7 @@
 	$host = "freescore.net";
 	$tournament = json_encode( [ 
 		"name" => "Central Valley Poomsae Challenge",
-		"db" => "cv-poomsae", 
+		"db" => "tma-2015-spring", 
 		"rings" => [ "count" => 2, "width" => 2, "height" => 1, "formation" => "rows" ],
 	]);
 ?>
