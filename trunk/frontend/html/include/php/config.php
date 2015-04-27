8688d18bf9dc5e9188ced91770d04b7f274b46d8
mikewongtkd <mikewongtkd@gmail.com>
2015-04-26 23:19:51 -0700

    Working version on Raspberry Pi

--- trunk/frontend/html/include/php/config.php
+++ trunk/frontend/html/include/php/config.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "192.168.1.109";
+	$host = "freescore.net";
 	$tournament = json_encode( [ 
-		"name" => "UC Open 2015",
-		"db" => "tma-2015-spring", 
+		"name" => "FreeScore",
+		"db" => "freescore", 
 		"rings" => [ "count" => 2, "width" => 2, "height" => 1, "formation" => "rows" ],
 	]);
 ?>
