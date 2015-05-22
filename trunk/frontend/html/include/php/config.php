5c21f0ade0b5e627aa2a2d27e7e76b057f190702
mikewongtkd <mikewongtkd@gmail.com>
2015-05-22 07:41:45 -0700

    Somehow the config file corrupted the SVN database (malformed XML errors)

--- trunk/frontend/html/include/php/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$host = "freescore.net";
-	$tournament = json_encode( [ 
-		"name" => "FreeScore Test Data",
-		"db" => "test", 
-		"rings" => [ "count" => 2, "width" => 2, "height" => 1, "formation" => "rows" ]
-	]);
-?>
