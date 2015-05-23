a8061a027356c09c12ab37d74cc55b1b6e8d5b15
Mike Wong <mikewong899@me.com>
2015-05-22 20:31:20 -0700

    Improved formatting of error messages.

--- trunk/frontend/html/include/js/jquery.errormessage.js
+++ trunk/frontend/html/include/js/jquery.errormessage.js
@@ -14,25 +14,31 @@ $.widget( "freescore.errormessage", {
 		var w = this.element;
 		var html = o.html;
 
-		var text = o.message.split( ':', 2 );
+		var text  = o.message.split( ':', 2 );
 		var error = { title: text[ 0 ], message: text[ 1 ] };
+		var code  = html.p.clone() .css( "color", "#fc0" ) .css( "font-family", "Monaco" );
 
-		error.message = error.message.replace( /'([^']+)'/, "<code>$1</code>" );
+		error.message = error.message.replace( /Can't/, "Can&apos;t" );
+		error.message = error.message.replace( /'([^']+)'/,
+		function( text ) {
+			text = text.replace( /'/g, '' );
+			return code.clone() .html( text ) [ 0 ].outerHTML;
+		});
 		error.message = error.message.replace( /at \/usr\/local\/freescore\/lib\/([\w\/]+\.pm) line (\d+)[^_]*$/, 
 		function( orig, module, line ) {
 			module = module.replace( /\//g, "::" );
 			module = module.replace( /\.pm/, "" );
-			return "at line " + line + " in module <code>" + module + "</code>";
+			return "at line " + line + " in module " + code.clone().html( module ) [ 0 ].outerHTML;
 		});
 		
 		var dialog = html.div.clone() .attr( "data-role", "page" ) .attr( "data-dialog", "true" ) .attr( "data-close-btn", "none" );
-		var header = html.div.clone() .attr( "data-role", "header" ) .attr( "data-position", "fixed" ) .attr( "data-theme", "b" ) .addClass( "header" );
+		var header = html.div.clone() .attr( "data-role", "header" ) .attr( "data-position", "fixed" ) .css( "color", "white" ) .css( "background", "#900" ) .css( "text-shadow", "none" ) .addClass( "header" );
 		var h1     = html.h1.clone();
-		var p      = html.p.clone();
-		var body   = html.div.clone() .attr( "role", "main" ) .addClass( "ui-content" ) .css( "background", "#eee" );
+		var p      = html.p.clone() .css( "font-size", "14pt" );
+		var body   = html.div.clone() .attr( "role", "main" ) .addClass( "ui-content" ) .css( "background", "#333" ) .css( "text-shadow", "none" );
 
 		header.append( h1.clone().html( "Error" ));
-		body.append( h1.clone().html( error.title ), p.html( error.message ));
+		body.append( h1.clone().html( error.title ) .css( "color", "#f90" ), p.html( error.message ) .css( "color", "white" ));
 		dialog.append( header, body );
 		dialog.enhanceWithin();
 		w.empty();
