5e83b18a3eeb1b095abc58216b7b57e2d88becfb
Mike Wong <mikewong@sfsu.edu>
2015-08-03 18:19:59 -0700

    Adding splash page.

---
+++ trunk/frontend/html/splash.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<meta http-equiv="refresh" content="1;url=http://freescore.net/freescore/index.php">
+		<title>FreeScore Wifi</title>
+		<style type="text/css">
+			body         { background-color: black; }
+			.splash      { position: absolute; top: calc( 50% - 202px ); left: calc( 50% - 360px ); width: 720px; height: 405px; background: url('/freescore/images/bg/team.jpg'); }
+			.callout     { position: absolute; top: 200px; left: 40px; background-color: rgba( 0,0,0,0.75 ); padding: 0 20px 10px 20px; border-radius: 24px; }
+			.title       { color: yellow; font-family: HelveticaNeue-CondensedBold; font-size: 72pt; text-shadow: 4px 4px #630; }
+			.description { color: white;  font-family: helvetica, arial, sans-serif; font-size: 12pt; }
+			.author      { color: #999; font-family: helvetica, arial, sans-serif; font-size: 9pt; padding-top: 8px; }
+		</style>
+	</head>
+	<body style="background-color: black;">
+		<div class="splash">
+			<div class="callout">
+				<div class="title">FreeScore</div>
+				<div class="description">Open Source Taekwondo Poomsae Scoring Software</div>
+				<div class="author">by Mike Wong &copy;2015 All Rights Reserved</div>
+			</div>
+		</div>
+	</body>
+</html>
