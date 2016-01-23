553ebcd6b984fe8958fe57b91c4a8936fd9d1ef9
Mike Wong <mikewong@sfsu.edu>
2016-01-23 11:23:29 -0800

    Progress files for rings now have proper permissions when created.

--- trunk/frontend/html/include/php/config.php
+++ trunk/frontend/html/include/php/config.php
@@ -3,6 +3,6 @@
 	$tournament = json_encode( [ 
 		"name" => "FreeScore Test Data",
 		"db" => "test", 
-		"rings" => [ "count" => 6, "start" => 1, "enable" => [ 1, 2, 5, 6 ], "width" => 3, "height" => 2, "formation" => "loop" ]
+		"rings" => [ "count" => 4, "start" => 1, "enable" => [ 1, 2, 3, 4 ], "width" => 2, "height" => 2, "formation" => "rows" ]
 	]);
 ?>
