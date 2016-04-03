ccf6dff4e266d8c1eac4705640aaba2adb6a6be3
Mike Wong <mikewong899@me.com>
2016-04-03 10:30:48 -0700

    Separating out dialogs from editor for easier development.

---
+++ trunk/frontend/html/forms/worldclass/division/dialog/poomsae-selection.php
@@ -0,0 +1,92 @@
+<div class="modal fade" id="modal-edit-forms" tabindex="-1" role="dialog" data-backdrop="static">
+	<div class="modal-dialog">
+		<div class="modal-content panel-primary">
+			<div class="modal-header panel-heading">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title">Edit Poomsae Selection</h4>
+			</div>
+			<div class="modal-body">
+				<h4>Select Competition Method</h4>
+				<div class="btn-group" data-toggle="buttons">
+					<label class="btn btn-success active">
+						<input type="radio" name="options" id="cutoff" autocomplete="off" checked> Cutoff
+					</label>
+					<label class="btn btn-success">
+						<input type="radio" name="options" id="modcut" autocomplete="off"> Extended Cutoff
+					</label>
+					<label class="btn btn-success">
+						<input type="radio" name="options" id="cutsin" autocomplete="off"> Cutoff/Single Elimination
+					</label>
+				</div>
+
+				<h4>Select Poomsae for each Round</h4>
+				<div style="margin-bottom: 10px;">
+					<div class="btn-group">
+						<button type="button" class="btn btn-primary btn-addon" style="width: 200px; text-align: left;">
+						Preliminary Round</button>
+						<div class="btn-group" role="group">
+							<button type="button" class="btn btn-default dropdown-toggle" id="prelim-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 220px; text-align: left;">
+							1st Form: None <span class="caret" style="float: right; margin-top:8px;"></span></button>
+							<ul class="dropdown-menu" id="prelim-1-select"> <li><a href="#">None</a></li> <li role="separator" class="divider"></li> <li><a href="#">Taegeuk 1</a></li> <li><a href="#">Taegeuk 2</a></li> <li><a href="#">Taegeuk 3</a></li> <li><a href="#">Taegeuk 4</a></li> <li><a href="#">Taegeuk 5</a></li> <li><a href="#">Taegeuk 6</a></li> <li><a href="#">Taegeuk 7</a></li> <li><a href="#">Taegeuk 8</a></li> <li role="separator" class="divider"></li> <li><a href="#">Koryo</a></li> <li><a href="#">Keumgang</a></li> <li><a href="#">Taebaek</a></li> <li><a href="#">Pyongwon</a></li> <li><a href="#">Sipjin</a></li> <li><a href="#">Jitae</a></li> <li><a href="#">Chongkwon</a></li> <li><a href="#">Hansu</a></li> </ul>
+						</div>
+						<div class="btn-group" role="group">
+							<button type="button" class="btn btn-default dropdown-toggle" id="prelim-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 220px; text-align: left;">
+							2nd Form: None <span class="caret" style="float: right; margin-top:8px;"></span></button>
+							<ul class="dropdown-menu" id="prelim-2-select"> <li><a>None</a></li> <li role="separator" class="divider"></li> <li><a>Taegeuk 1</a></li> <li><a>Taegeuk 2</a></li> <li><a>Taegeuk 3</a></li> <li><a>Taegeuk 4</a></li> <li><a>Taegeuk 5</a></li> <li><a>Taegeuk 6</a></li> <li><a>Taegeuk 7</a></li> <li><a>Taegeuk 8</a></li> <li role="separator" class="divider"></li> <li><a>Koryo</a></li> <li><a>Keumgang</a></li> <li><a>Taebaek</a></li> <li><a>Pyongwon</a></li> <li><a>Sipjin</a></li> <li><a>Jitae</a></li> <li><a>Chongkwon</a></li> <li><a>Hansu</a></li> </ul>
+						</div>
+					</div>
+				</div>
+
+				<div style="margin-bottom: 10px;">
+					<div class="btn-group">
+						<button type="button" class="btn btn-primary btn-addon" style="width: 200px; text-align: left;">
+						Semi-Finals Round</button>
+						<div class="btn-group" role="group">
+							<button type="button" class="btn btn-default dropdown-toggle" id="semfin-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 220px; text-align: left;">
+							1st Form: None <span class="caret" style="float: right; margin-top:8px;"></span></button>
+							<ul class="dropdown-menu" id="semfin-1-select"> <li><a href="#">None</a></li> <li role="separator" class="divider"></li> <li><a href="#">Taegeuk 1</a></li> <li><a href="#">Taegeuk 2</a></li> <li><a href="#">Taegeuk 3</a></li> <li><a href="#">Taegeuk 4</a></li> <li><a href="#">Taegeuk 5</a></li> <li><a href="#">Taegeuk 6</a></li> <li><a href="#">Taegeuk 7</a></li> <li><a href="#">Taegeuk 8</a></li> <li role="separator" class="divider"></li> <li><a href="#">Koryo</a></li> <li><a href="#">Keumgang</a></li> <li><a href="#">Taebaek</a></li> <li><a href="#">Pyongwon</a></li> <li><a href="#">Sipjin</a></li> <li><a href="#">Jitae</a></li> <li><a href="#">Chongkwon</a></li> <li><a href="#">Hansu</a></li> </ul>
+						</div>
+						<div class="btn-group" role="group">
+							<button type="button" class="btn btn-default dropdown-toggle" id="semfin-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 220px; text-align: left;">
+							2nd Form: None <span class="caret" style="float: right; margin-top:8px;"></span></button>
+							<ul class="dropdown-menu" id="semfin-2-select"> <li><a>None</a></li> <li role="separator" class="divider"></li> <li><a>Taegeuk 1</a></li> <li><a>Taegeuk 2</a></li> <li><a>Taegeuk 3</a></li> <li><a>Taegeuk 4</a></li> <li><a>Taegeuk 5</a></li> <li><a>Taegeuk 6</a></li> <li><a>Taegeuk 7</a></li> <li><a>Taegeuk 8</a></li> <li role="separator" class="divider"></li> <li><a>Koryo</a></li> <li><a>Keumgang</a></li> <li><a>Taebaek</a></li> <li><a>Pyongwon</a></li> <li><a>Sipjin</a></li> <li><a>Jitae</a></li> <li><a>Chongkwon</a></li> <li><a>Hansu</a></li> </ul>
+						</div>
+					</div>
+				</div>
+
+				<div style="margin-bottom: 10px;">
+					<div class="btn-group">
+						<button type="button" class="btn btn-primary btn-addon" style="width: 200px; text-align: left;">
+						Finals Round</button>
+						<div class="btn-group" role="group">
+							<button type="button" class="btn btn-default dropdown-toggle" id="finals-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 220px; text-align: left;">
+							1st Form: None <span class="caret" style="float: right; margin-top:8px;"></span></button>
+							<ul class="dropdown-menu" id="finals-1-select"> <li><a href="#">None</a></li> <li role="separator" class="divider"></li> <li><a href="#">Taegeuk 1</a></li> <li><a href="#">Taegeuk 2</a></li> <li><a href="#">Taegeuk 3</a></li> <li><a href="#">Taegeuk 4</a></li> <li><a href="#">Taegeuk 5</a></li> <li><a href="#">Taegeuk 6</a></li> <li><a href="#">Taegeuk 7</a></li> <li><a href="#">Taegeuk 8</a></li> <li role="separator" class="divider"></li> <li><a href="#">Koryo</a></li> <li><a href="#">Keumgang</a></li> <li><a href="#">Taebaek</a></li> <li><a href="#">Pyongwon</a></li> <li><a href="#">Sipjin</a></li> <li><a href="#">Jitae</a></li> <li><a href="#">Chongkwon</a></li> <li><a href="#">Hansu</a></li> </ul>
+						</div>
+						<div class="btn-group" role="group">
+							<button type="button" class="btn btn-default dropdown-toggle" id="finals-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 220px; text-align: left;">
+							2nd Form: None <span class="caret" style="float: right; margin-top:8px;"></span></button>
+							<ul class="dropdown-menu" id="finals-2-select"> <li><a>None</a></li> <li role="separator" class="divider"></li> <li><a>Taegeuk 1</a></li> <li><a>Taegeuk 2</a></li> <li><a>Taegeuk 3</a></li> <li><a>Taegeuk 4</a></li> <li><a>Taegeuk 5</a></li> <li><a>Taegeuk 6</a></li> <li><a>Taegeuk 7</a></li> <li><a>Taegeuk 8</a></li> <li role="separator" class="divider"></li> <li><a>Koryo</a></li> <li><a>Keumgang</a></li> <li><a>Taebaek</a></li> <li><a>Pyongwon</a></li> <li><a>Sipjin</a></li> <li><a>Jitae</a></li> <li><a>Chongkwon</a></li> <li><a>Hansu</a></li> </ul>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Save Changes</button>
+			</div>
+		</div>
+	</div>
+</div>
+<script>
+	$( ".dropdown-menu" ).find( "a" ).click( function() {
+		var poomsae  = $( this ).html();
+		var dropdown = $( this ).parents( ".dropdown-menu" ).attr( 'id' );
+		var button   = dropdown.replace( /-select/, '' );
+		var ordinal  = undefined;
+		if( button.match( /1$/ )) { ordinal = '1st form: '; } else if( button.match( /2$/ )) { ordinal = '2nd form: '; }
+		$( '#' + button ).html( ordinal + poomsae );
+
+	});
+</script>
--- trunk/frontend/html/forms/worldclass/division/editor.php
+++ trunk/frontend/html/forms/worldclass/division/editor.php
@@ -62,57 +62,8 @@
 
 			<button type="button" class="btn btn-success">Save Changes</button>
 
-			<div class="modal fade" id="modal-edit-forms" tabindex="-1" role="dialog" data-backdrop="static">
-				<div class="modal-dialog">
-					<div class="modal-content panel-primary">
-						<div class="modal-header panel-heading">
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-							<h4 class="modal-title">Edit Poomsae Selection</h4>
-						</div>
-						<div class="modal-body">
-							<div class="input-group" style="margin-bottom: 10px;">
-								<div class="input-group-btn">
-									<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 200px; text-align: left;">
-									Preliminary Round <span class="caret" style="float: right; margin-top:8px;"></span></button>
-									<ul class="dropdown-menu"> <li><a href="#">None</a></li> <li role="separator" class="divider"></li> <li><a href="#">Taegeuk 1</a></li> <li><a href="#">Taegeuk 2</a></li> <li><a href="#">Taegeuk 3</a></li> <li><a href="#">Taegeuk 4</a></li> <li><a href="#">Taegeuk 5</a></li> <li><a href="#">Taegeuk 6</a></li> <li><a href="#">Taegeuk 7</a></li> <li><a href="#">Taegeuk 8</a></li> <li role="separator" class="divider"></li> <li><a href="#">Koryo</a></li> <li><a href="#">Keumgang</a></li> <li><a href="#">Taebaek</a></li> <li><a href="#">Pyongwon</a></li> <li><a href="#">Sipjin</a></li> <li><a href="#">Jitae</a></li> <li><a href="#">Chongkwon</a></li> <li><a href="#">Hansu</a></li> </ul>
-								</div>
-								<input type="text" class="form-control" aria-label="poomsae selection">
-							</div>
+			<?php include( "dialog/poomsae-selection.php" ) ?>
 
-							<div class="input-group" style="margin-bottom: 10px;">
-								<div class="input-group-btn">
-									<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 200px; text-align: left;">
-									Semi-finals Round <span class="caret" style="float: right; margin-top:8px;"></span></button>
-									<ul class="dropdown-menu"> <li><a href="#">None</a></li> <li role="separator" class="divider"></li> <li><a href="#">Taegeuk 1</a></li> <li><a href="#">Taegeuk 2</a></li> <li><a href="#">Taegeuk 3</a></li> <li><a href="#">Taegeuk 4</a></li> <li><a href="#">Taegeuk 5</a></li> <li><a href="#">Taegeuk 6</a></li> <li><a href="#">Taegeuk 7</a></li> <li><a href="#">Taegeuk 8</a></li> <li role="separator" class="divider"></li> <li><a href="#">Koryo</a></li> <li><a href="#">Keumgang</a></li> <li><a href="#">Taebaek</a></li> <li><a href="#">Pyongwon</a></li> <li><a href="#">Sipjin</a></li> <li><a href="#">Jitae</a></li> <li><a href="#">Chongkwon</a></li> <li><a href="#">Hansu</a></li> </ul>
-								</div>
-								<input type="text" class="form-control" aria-label="poomsae selection">
-							</div>
-
-							<div class="input-group" style="margin-bottom: 10px;">
-								<div class="input-group-btn">
-									<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 200px; text-align: left;">
-									Finals Round 1st Form <span class="caret" style="float: right; margin-top:8px;"></span></button>
-									<ul class="dropdown-menu"> <li><a href="#">None</a></li> <li role="separator" class="divider"></li> <li><a href="#">Taegeuk 1</a></li> <li><a href="#">Taegeuk 2</a></li> <li><a href="#">Taegeuk 3</a></li> <li><a href="#">Taegeuk 4</a></li> <li><a href="#">Taegeuk 5</a></li> <li><a href="#">Taegeuk 6</a></li> <li><a href="#">Taegeuk 7</a></li> <li><a href="#">Taegeuk 8</a></li> <li role="separator" class="divider"></li> <li><a href="#">Koryo</a></li> <li><a href="#">Keumgang</a></li> <li><a href="#">Taebaek</a></li> <li><a href="#">Pyongwon</a></li> <li><a href="#">Sipjin</a></li> <li><a href="#">Jitae</a></li> <li><a href="#">Chongkwon</a></li> <li><a href="#">Hansu</a></li> </ul>
-								</div>
-								<input type="text" class="form-control" aria-label="poomsae selection">
-							</div>
-
-							<div class="input-group" style="margin-bottom: 10px;">
-								<div class="input-group-btn">
-									<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width: 200px; text-align: left;">
-									Finals Round 2nd Form <span class="caret" style="float: right; margin-top:8px;"></span></button>
-									<ul class="dropdown-menu"> <li><a href="#">None</a></li> <li role="separator" class="divider"></li> <li><a href="#">Taegeuk 1</a></li> <li><a href="#">Taegeuk 2</a></li> <li><a href="#">Taegeuk 3</a></li> <li><a href="#">Taegeuk 4</a></li> <li><a href="#">Taegeuk 5</a></li> <li><a href="#">Taegeuk 6</a></li> <li><a href="#">Taegeuk 7</a></li> <li><a href="#">Taegeuk 8</a></li> <li role="separator" class="divider"></li> <li><a href="#">Koryo</a></li> <li><a href="#">Keumgang</a></li> <li><a href="#">Taebaek</a></li> <li><a href="#">Pyongwon</a></li> <li><a href="#">Sipjin</a></li> <li><a href="#">Jitae</a></li> <li><a href="#">Chongkwon</a></li> <li><a href="#">Hansu</a></li> </ul>
-								</div>
-								<input type="text" class="form-control" aria-label="poomsae selection">
-							</div>
-						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-							<button type="button" class="btn btn-primary">Save Changes</button>
-						</div>
-					</div><!-- /.modal-content -->
-				</div><!-- /.modal-dialog -->
-			</div><!-- /.modal -->
 		</div>
 	</body>
 </html>
