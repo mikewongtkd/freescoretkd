908f6a778ff67c008030f11d77999f6f0fd32d6b
Mike Wong <mikewong899@me.com>
2016-09-20 03:19:46 -0700

    Thinking about div id scheme based on description. See comments in description.php.

--- trunk/frontend/html/forms/worldclass/division/description.php
+++ trunk/frontend/html/forms/worldclass/division/description.php
@@ -16,28 +16,28 @@
 						<label class="btn btn-default"><input type="radio" name="gender" value="m">Male</label>
 					</div>
 					<div class="btn-group" data-toggle="buttons">
-						<label class="btn btn-default"><input type="radio" name="age" value="6-7"     >6-7</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="8-9"     >8-9</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="youths"  >10-11</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="cadets"  >12-14</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="juniors" >15-17</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="under 30">18-29</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="under 40">30-39</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="under 50">40-49</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="under 60">50-59</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="under 65">60-65</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="over 65" >66+</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="02" value="6-7"     >6-7</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="03" value="8-9"     >8-9</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="04" value="youths"  >10-11</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="05" value="cadets"  >12-14</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="06" value="juniors" >15-17</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="07" value="under 30">18-29</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="08" value="under 40">30-39</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="09" value="under 50">40-49</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="10" value="under 60">50-59</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="11" value="under 65">60-65</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="12" value="over 65" >66+</label>
 					</div>
 				</div>
 				<div class="tab-pane" id="pair">
 					<div class="btn-group" data-toggle="buttons">
-						<label class="btn btn-default"><input type="radio" name="age" value="6-9"     >6-9</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="youths"  >10-11</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="cadets"  >12-14</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="juniors" >15-17</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="under 30">18-29</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="under 40">30-39</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="over 40" >40+</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="02" value="6-9"     >6-9</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="03" value="youths"  >10-11</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="04" value="cadets"  >12-14</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="05" value="juniors" >15-17</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="06" value="under 30">18-29</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="07" value="under 40">30-39</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="08" value="over 40" >40+</label>
 					</div>
 				</div>
 				<div class="tab-pane" id="team">
@@ -46,13 +46,13 @@
 						<label class="btn btn-default"><input type="radio" name="gender" value="m">Male</label>
 					</div>
 					<div class="btn-group" data-toggle="buttons">
-						<label class="btn btn-default"><input type="radio" name="age" value="6-9"     >6-9</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="youths"  >10-11</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="cadets"  >12-14</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="juniors" >15-17</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="under 30">18-29</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="under 40">30-39</label>
-						<label class="btn btn-default"><input type="radio" name="age" value="over 40" >40+</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="02" value="6-9"     >6-9</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="03" value="youths"  >10-11</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="04" value="cadets"  >12-14</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="05" value="juniors" >15-17</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="06" value="under 30">18-29</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="07" value="under 40">30-39</label>
+						<label class="btn btn-default"><input type="radio" name="age" idx="08" value="over 40" >40+</label>
 					</div>
 				</div>
 			</div>
@@ -62,7 +62,11 @@
 
 <script>
 	// ===== DESCRIPTION BEHAVIOR
-	var description = { category: '', gender: '', age: '', text: '', update : function() { 
+	// Individuals: p01-p50
+	// Pairs:       p51-p60
+	// Team:        p61-p80
+	// id = gen==f ? 2 x idx -1 : 2 x idx; indv += 0; pair += 50; team += 60
+	var description = { category: '', gender: '', age: '', text: '', divid: 0, update : function() { 
 		description.text = { m:'Male', f:'Female', '':'' }[ description.gender ] + ' ' + description.category.capitalize() + ' ' + description.age.capitalize();
 		description.text = description.text.trim();
 		description.text = description.text.replace( /\s+/, ' ' );
