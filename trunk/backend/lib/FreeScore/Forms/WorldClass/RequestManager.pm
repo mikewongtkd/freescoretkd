d13b6b360af16d80f6e3f2c41018cb48669a7520
Mike Wong <mikewong899@me.com>
2016-10-24 01:01:15 -0700

    Adding webservices for detecting and providing updates.

--- trunk/backend/lib/FreeScore/Forms/WorldClass/RequestManager.pm
+++ trunk/backend/lib/FreeScore/Forms/WorldClass/RequestManager.pm
@@ -2,6 +2,7 @@ package FreeScore::Forms::WorldClass::RequestManager;
 use lib qw( /usr/local/freescore/lib );
 use Try::Tiny;
 use FreeScore;
+use FreeScore::Repository;
 use FreeScore::Forms::WorldClass;
 use JSON::XS;
 use Digest::SHA1 qw( sha1_hex );
@@ -29,6 +30,10 @@ sub init {
 	$self->{ _autopilot }  = shift;
 	$self->{ _json }       = new JSON::XS();
 	$self->{ _watching }   = {};
+	$self->{ software }    = {
+		check_updates      => \&handle_software_check_updates,
+		update             => \&handle_software_update
+	};
 	$self->{ division }    = {
 		athlete_delete     => \&handle_division_athlete_delete,
 		athlete_next       => \&handle_division_athlete_next,
@@ -714,6 +719,45 @@ sub handle_ring_transfer {
 		$progress->transfer( $divid, $transfer );
 
 		$self->broadcast_ring_response( $request, $progress, $clients );
+	} catch {
+		$client->send( { json => { error => "$_" }});
+	}
+}
+
+# ============================================================
+sub handle_software_check_updates {
+# ============================================================
+	my $self     = shift;
+	my $request  = shift;
+	my $progress = shift;
+	my $clients  = shift;
+	my $judges   = shift;
+
+	try {
+		my $repo    = new FreeScore::Repository();
+		my $latest  = $repo->latest_release();
+		my $current = $repo->local_version();
+		my $update  = $current < $latest;
+
+		$client->send( { json => { type => $request->{ type }, action => 'updates', available => $update  }});
+
+	} catch {
+		$client->send( { json => { error => "$_" }});
+	}
+}
+
+# ============================================================
+sub handle_software_update {
+# ============================================================
+	my $self     = shift;
+	my $request  = shift;
+	my $progress = shift;
+	my $clients  = shift;
+	my $judges   = shift;
+
+	try {
+		my $repo = new FreeScore::Repository();
+		$repo->update_local_to_latest();
 	} catch {
 		$client->send( { json => { error => "$_" }});
 	}
---
+++ trunk/backend/lib/FreeScore/Repository.pm
@@ -0,0 +1,71 @@
+package FreeScore::Repository;
+use Data::Dumper;
+
+# ============================================================
+sub new {
+# ============================================================
+	my ($class) = map { ref || $_ } shift;
+	my $self = bless {}, $class;
+	$self->init( @_ );
+	return $self;
+}
+
+# ============================================================
+sub init {
+# ============================================================
+	my $self = shift;
+
+	$self->{ _url } = "https://github.com/mikewongtkd/freescoretkd.git";
+
+	my $pi    = "~pi/freescore/trunk";
+	my $devel = "~/devel/freescore/trunk";
+
+	$self->{ _local } = -e $pi ? $pi : $devel;
+}
+
+# ============================================================
+sub latest_release {
+# ============================================================
+	my $self     = shift;
+	my $response = `git ls-remote --tags $self->{ _url } 2>&1`;
+
+	if( $response =~ /\bfatal\b/ ) { chomp $response; die "Network error \"$response\"$!" ; }
+
+	my @versions = split /\n/, $response;
+
+	@versions = sort { $b <=> $a } map { 
+		chomp;
+		my ($hash, $tag) = split /\t/;
+		$tag =~ s/^.*\/v//;
+		$tag =~ /^(\d+\.\d+)$/ ? $1 : ();
+	} @versions;
+
+	my $latest = shift @versions;
+
+	return $latest;
+}
+
+# ============================================================
+sub local_version {
+# ============================================================
+	$self        = shift;
+	my $response = `cd $self->{ _local } && git describe --tags --abbrev=0 2>&1`;
+
+	chomp $response;
+	if( $response =~ /\bfatal\b/ ) { die "Error: \"$response\"$!"; }
+
+	$response =~ s/^v//;
+	return $response;
+}
+
+# ============================================================
+sub update_local_to_latest {
+# ============================================================
+	my $self   = shift;
+
+	my $latest   = $self->latest_release();
+	my $response = `cd $self->{ _local } && git pull +ref/tags/v$latest 2>&1`;
+	if( $response =~ /\bfatal\b/ ) { chomp $response; die "Error: \"$response\"$!"; }
+}
+
+1;
