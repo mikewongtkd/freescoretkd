5a805b401802e2b96e0ea4e95b24ecf172681123
Mike Wong <mikewongtkd@gmail.com>
2016-11-05 00:41:29 -0700

    Styling the grassroots registration correctly

--- trunk/frontend/html/forms/grassroots/register.php
+++ trunk/frontend/html/forms/grassroots/register.php
@@ -5,10 +5,14 @@
 <html>
 	<head>
 		<link href="../../include/css/setup/register.css" rel="stylesheet" />
+		<link href="../../include/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="../../include/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" />
 		<script src="../../include/jquery/js/jquery.js"></script>
 		<script src="../../include/jquery/js/jquery-ui.min.js"></script>
 		<script src="../../include/jquery/js/jquery.cookie.js"></script>
 		<script src="../../include/jquery/js/jquery.purl.js"></script>
+		<script src="../../include/bootstrap/js/bootstrap.min.js"></script>
+		<script src="../../include/bootstrap/add-ons/bootbox.min.js"></script>
 		<script src="../../include/js/freescore.js"></script>
 		<script src="../../include/js/forms/grassroots/jquery.register.js"></script>
 	</head>
