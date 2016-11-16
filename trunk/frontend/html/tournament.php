4840fd698d20bb437dc438143a7bef9ad59fe818
Mike Wong <mikewong899@me.com>
2016-11-15 22:06:55 -0800

    Tournament form initial setup.

--- trunk/frontend/html/tournament.php
+++ trunk/frontend/html/tournament.php
@@ -4,9 +4,9 @@
 ?>
 <html>
 	<head>
-		<title>Tournament Configuration</title>
+		<title>Tournament Setup</title>
 		<link href="./include/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-		<link href="./include/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" />
+		<link href="./include/bootstrap/css/freescore-theme.min.css" rel="stylesheet" />
 		<script src="./include/jquery/js/jquery.js"></script>
 		<script src="./include/jquery/js/jquery.howler.min.js"></script>
 		<script src="./include/bootstrap/js/bootstrap.min.js"></script>
@@ -27,46 +27,56 @@
 	<body>
 		<div class="container">
 			<div class="page-header">
-				<h1>FreeScore TKD Wifi <small>v<?=$freescore[ 'version' ]?></small></h1>
-				<p><a href="http://mikewongtkd.github.io/freescoretkd">Open Source Taekwondo Poomsae Scoring Software</a> available under the GPL v2.</p>
+				<h1>Tournament Setup</h1>
 			</div>
+
 			<div class="panel panel-primary">
 				<div class="panel-heading">
-					<h1 class="panel-title">Sport Poomsae: Score poomsae following the WTF Sport Poomsae rules</h1>
+					<h1 class="panel-title">Select the setup for this FreeScore WiFi Server</h1>
 				</div>
 				<div class="panel-body">
-					<div class="list-group">
-						<a class="list-group-item" href="./forms/worldclass/register.php">
-							<h4>Register a Tablet or Computer for Sport Poomsae</h4>
-							<p>Assign and ring and role for a ring laptop or judge tablet</p>
-						</a>
-						<a class="list-group-item" href="./forms/worldclass/divisions.php">
-							<h4>Manage Sport Poomsae Divisions</h4>
-							<p>Add, remove, or edit divisions; also add or remove rings</p>
-						</a>
-					</div>
+					<form>
+						<div class="form-group row">
+							<label for="name" class="col-xs-2 col-form-label">Tournament Name</label>
+							<div class="col-xs-10">
+								<input class="form-control" type="text" id="name">
+							</div>
+						</div>
+						<div class="form-group row">
+							<label for="rings" class="col-xs-2 col-form-label">Rings</label>
+							<div class="col-xs-10">
+								<div class="btn-group" data-toggle="buttons-checkbox">
+									<button type="button" class="btn btn-primary">1</button>
+									<button type="button" class="btn btn-primary">2</button>
+									<button type="button" class="btn btn-primary">3</button>
+									<button type="button" class="btn btn-primary">4</button>
+									<button type="button" class="btn btn-primary">5</button>
+									<button type="button" class="btn btn-primary">6</button>
+									<button type="button" class="btn btn-primary">7</button>
+									<button type="button" class="btn btn-primary">8</button>
+									<button type="button" class="btn btn-primary">9</button>
+									<button type="button" class="btn btn-primary">10</button>
+									<button type="button" class="btn btn-primary">11</button>
+									<button type="button" class="btn btn-primary">12</button>
+									<button type="button" class="btn btn-primary">13</button>
+									<button type="button" class="btn btn-primary">14</button>
+									<button type="button" class="btn btn-primary">15</button>
+									<button type="button" class="btn btn-primary">16</button>
+									<button type="button" class="btn btn-primary">17</button>
+									<button type="button" class="btn btn-primary">18</button>
+									<button type="button" class="btn btn-primary">19</button>
+									<button type="button" class="btn btn-primary">20</button>
+								</div>
+							</div>
+						</div>
+
+						<div class="clearfix">
+							<button type="button" id="cancel" class="btn btn-danger  pull-left" >Cancel</button> 
+							<button type="button" id="accept" class="btn btn-success pull-right">Accept</button> 
+						</div>
+					</form>
 				</div>
 			</div>
-			<div class="panel panel-primary">
-				<div class="panel-heading">
-					<h1 class="panel-title">Classic Poomsae: Score poomsae using an electronic version of the classic flip cards</h1>
-				</div>
-				<div class="panel-body">
-					<div class="list-group">
-						<a class="list-group-item" href="./forms/grassroots/register.php">
-							<h4>Register a Tablet or Computer for Classic Poomsae</h4>
-							<p>Assign and ring and role for a ring laptop or judge tablet</p>
-						</a>
-						<a class="list-group-item" href="./forms/grassroots/divisions.php">
-							<h4>Manage Classic Poomsae Divisions</h4>
-							<p>Add, remove, or edit divisions; also add or remove rings</p>
-						</a>
-					</div>
-				</div>
-			</div>
-			<footer class="page-footer">
-				<p class="text-muted">&copy; <?= $freescore[ 'copyright' ] ?> Mike Wong All Rights Reserved.</p>
-			</footer>
 		</div>
 		<script>
 
@@ -86,34 +96,20 @@ $( '.list-group a' ).click( function( ev ) {
 var host       = '<?= $host ?>';
 var tournament = <?= $tournament ?>;
 
-var ws = new WebSocket( 'ws://' + host + ':3088/worldclass/' + tournament.db + '/staging' );
-
-ws.onopen = function() {
-	var request = { data : { type : 'software', action : 'check updates' }};
-	request.json = JSON.stringify( request.data );
-	ws.send( request.json );
-};
+/* var ws = new WebSocket( 'ws://' + host + ':3088/worldclass/' + tournament.db + '/staging' );
 
 ws.onmessage = function( response ) {
 	var update = JSON.parse( response.data );
-	if( update.type == 'software' ) {
-		if( update.available ) {
-			sound.send.play();
-			bootbox.confirm({
-				title:   "New version " + update.version + " is available.",
-				message: "Current installed version is " + update.current + ". Click OK to download and install update version " + update.version + ".",
-				callback: function( ok ) {
-					if( ok ) {
-						var request = { data : { type : 'software', action : 'update' }};
-						request.json = JSON.stringify( request.data );
-						ws.send( request.json );
-						sound.confirmed.play();
-					}
-				}
-			});
-		}
-	}
 };
+*/
+
+$( '#name' ).val( tournament.name );
+$( '.btn-group button' ).removeClass( 'active' );
+$.each( tournament.rings, (i, r) => {
+	$( '.btn-group button' ).each(( j, b ) => { if( parseInt( $( b ).html()) == r ) { $( b ).addClass( 'active' ); } });
+});
+$( '#cancel' ).off( 'click' ).click(() => { location = "index.php"; });
+$( '#accept' ).off( 'click' ).click(() => { console.log( "Submitting" ); });
 		</script>
 	</body>
 </html>
