804313256d5a7846b0d61e424aff220b63a8ff62
Mike Wong <mikewongtkd@gmail.com>
2018-07-25 02:31:43 -0700

    Started to update test system, realized I need a new test system probably written in Node.js. So using the client test page for now, will hold off on revamping test system later.

--- trunk/backend/test/include/php/config.php
+++ trunk/backend/test/include/php/config.php
@@ -1,8 +1,21 @@
 <?php
-	$host = "freescore.net";
-	$tournament = json_encode( [ 
-		"name" => "FreeScore Test Data",
-		"db" => "test", 
-		"rings" => [ "count" => 2, "width" => 2, "height" => 1, "formation" => "rows" ]
-	]);
+	function get_ring_number( $n ) {
+		if( ! preg_match( '/ring/', $n )) { return null; }
+		$n = preg_replace( '/ring/', '', $n );
+		return intval( $n );
+	};
+
+	$host       = "freescore.net";
+	$tournament = [ 
+		"name" => "FreeScore",
+		"db"   => "test", 
+	];
+	$rings = [];
+	$rings[ 'grassroots' ] = preg_grep( '/ring|staging/', scandir( '/usr/local/freescore/data/' . $tournament[ 'db' ] . '/forms-grassroots' ));
+	$rings[ 'worldclass' ] = preg_grep( '/ring|staging/', scandir( '/usr/local/freescore/data/' . $tournament[ 'db' ] . '/forms-worldclass' ));
+	$rings[ 'freestyle' ]  = preg_grep( '/ring|staging/', scandir( '/usr/local/freescore/data/' . $tournament[ 'db' ] . '/forms-freestyle'  ));
+	$rings = array_values( array_filter( array_map( 'get_ring_number', array_unique( array_merge( $rings[ 'grassroots' ], $rings[ 'worldclass' ], $rings[ 'freestyle' ] )))));
+	asort( $rings );
+	$tournament[ 'rings' ] = $rings;
+	$tournament = json_encode( $tournament );
 ?>
