c605070fd3b1e0989acee1419224fca4c35fe479
Mike Wong <mikewongtkd@gmail.com>
2019-01-24 08:39:01 -0800

    Updated dependencies in anticipation of use with Mac, since the EXTERN.h headers are no longer provided in XTools.

--- trunk/backend/raspberry-pi/rpi3/dependencies.pl
+++ trunk/backend/raspberry-pi/rpi3/dependencies.pl
@@ -1,5 +1,7 @@
 #! /usr/bin/perl
-use List::MoreUtils qw( uniq );
+use List::Util qw( first uniq );
+
+sub which ($);
 
 my $contains = {
 	'GD::Barcode::UPCE' => 'GD::Barcode',
@@ -19,7 +21,7 @@ push @modules,
 		exists $contains->{ $_ } ? $contains->{ $_ } : $_;
 	} `cd ../../ && grep -hir '^use' bin lib ../frontend/cgi-bin | sort | uniq`;
 
-my ($perl) = grep { -e $_ } ( `which perl`, '/usr/bin/perl', '/usr/local/bin/perl' );
+my $perl = first { -e $_ } ( which 'perl', '/usr/bin/perl', '/usr/local/bin/perl' );
 my @missing = grep {
 	my $test  = "$perl -M$_ -e 'exit();'";
 	my $error = system( "$test 2>/dev/null" );
@@ -27,31 +29,44 @@ my @missing = grep {
 } uniq sort @modules;
 
 # Test for network
-my ($ping) = grep { -e $_ } ( `which ping`, '/sbin/ping', '/bin/ping' );
+my ($ping) = grep { -e $_ } ( which 'ping', '/sbin/ping', '/bin/ping' );
 die "Can't test network" if( ! -e $ping );
 my $error = system( "$ping -c 1 cpan.org" );
 die "Not connected to the network" if( $error );
 
 # Network connected, try to install missing packages
-my ($cpanm) = grep { -e $_ } ( `which cpanm`, '/usr/bin/cpanm', '/usr/local/bin/cpanm' );
+my ($cpanm) = grep { -e $_ } ( which 'cpanm', '/usr/bin/cpanm', '/usr/local/bin/cpanm' );
 
 if( ! $cpanm ) {
-	my ($curl) = grep { -e $_ } ( `which curl`, '/usr/bin/curl', '/usr/local/bin/curl' );
+	my ($curl) = grep { -e $_ } ( which 'curl', '/usr/bin/curl', '/usr/local/bin/curl' );
 	if( ! $curl ) {
-		my ($aptget) = grep { -e $_ } ( `which apt-get`, '/sbin/apt-get', '/usr/bin/apt-get', '/usr/local/bin/apt-get' );
+		my ($aptget) = grep { -e $_ } ( which 'apt-get', '/sbin/apt-get', '/usr/bin/apt-get', '/usr/local/bin/apt-get' );
 		$error = system( "$aptget install -y curl" );
-		($curl) = grep { -e $_ } ( `which curl`, '/usr/bin/curl', '/usr/local/bin/curl' );
+		($curl) = grep { -e $_ } ( which 'curl', '/usr/bin/curl', '/usr/local/bin/curl' );
 		die "Failed to install required libraries" if( ! $curl || $error );
 	}
 	$error = system( "curl -L https://cpanmin.us | perl - --sudo App::cpanminus" );
-	my ($cpanm) = grep { -e $_ } ( `which cpanm`, '/usr/bin/cpanm', '/usr/local/bin/cpanm' );
+	my ($cpanm) = grep { -e $_ } ( which 'cpanm', '/usr/bin/cpanm', '/usr/local/bin/cpanm' );
 	die "Failed to install required libraries" if( ! -e $cpanm || $error );
 }
 
-my @errors = $error;
+my @errors = ( $error );
 foreach my $package (@missing) {
-	$error = system( "$cpanm $package" );
+	$error = system( "$cpanm --sudo $package" );
 	push @errors, $error if( $error );
 }
 
-print map { "$_\n" } @errors;
+if( @errors ) {
+	print map { "$_\n" } @errors;
+	exit( 1 );
+} else {
+	print "All dependencies satisfied.\n";
+	exit( 0 );
+}
+
+sub which ($) {
+	my $command = shift;
+	my $which = `which $command`;
+	chomp $which;
+	return $which;
+}
