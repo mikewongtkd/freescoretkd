fba3511637cf9a46ee47538c2eae03b900a448d9
Mike Wong <mikewongtkd@gmail.com>
2019-01-29 06:26:33 -0800

    Adding RCS and SSHD to dependencies

--- trunk/backend/raspberry-pi/rpi3/dependencies.pl
+++ trunk/backend/raspberry-pi/rpi3/dependencies.pl
@@ -1,6 +1,7 @@
 #! /usr/bin/perl
 use List::Util qw( first uniq );
 
+sub bin ($);
 sub which ($);
 
 my $contains = {
@@ -8,65 +9,128 @@ my $contains = {
 	'Mojolicious::Lite' => 'Mojolicious'
 };
 
-my @modules = ();
+our $perl    = bin 'perl' || die "Perl not installed";
+my  @modules = required_perl_modules();
+my  @missing = missing_perl_modules( @missing );
 
-push @modules,
-	grep { !/^(?:base|parent|warnings|strict|lib|Data::Dumper|FreeScore)/ } 
-	map { 
-		chomp;
-		s/^use\s*//;
-		s/\s\(?qw\([^)]+\)\)?//;
-		s/;$//;
-		s/\(\)$//;
-		exists $contains->{ $_ } ? $contains->{ $_ } : $_;
-	} `cd ../../ && grep -hir '^use' bin lib ../frontend/cgi-bin | sort | uniq`;
+test_for_network();
 
-my $perl = first { -e $_ } ( which 'perl', '/usr/bin/perl', '/usr/local/bin/perl' );
-my @missing = grep {
-	my $test  = "$perl -M$_ -e 'exit();'";
-	my $error = system( "$test 2>/dev/null" );
-	$error;
-} uniq sort @modules;
+my @errors  = install_missing_modules( @missing );
 
-# Test for network
-my ($ping) = grep { -e $_ } ( which 'ping', '/sbin/ping', '/bin/ping' );
-die "Can't test network" if( ! -e $ping );
-my $error = system( "$ping -c 1 cpan.org" );
-die "Not connected to the network" if( $error );
+install_history_system();
+configure_system_services();
 
-# Network connected, try to install missing packages
-my ($cpanm) = grep { -e $_ } ( which 'cpanm', '/usr/bin/cpanm', '/usr/local/bin/cpanm' );
-
-if( ! $cpanm ) {
-	my ($curl) = grep { -e $_ } ( which 'curl', '/usr/bin/curl', '/usr/local/bin/curl' );
-	if( ! $curl ) {
-		my ($aptget) = grep { -e $_ } ( which 'apt-get', '/sbin/apt-get', '/usr/bin/apt-get', '/usr/local/bin/apt-get' );
-		$error = system( "$aptget install -y curl" );
-		($curl) = grep { -e $_ } ( which 'curl', '/usr/bin/curl', '/usr/local/bin/curl' );
-		die "Failed to install required libraries" if( ! $curl || $error );
-	}
-	$error = system( "curl -L https://cpanmin.us | perl - --sudo App::cpanminus" );
-	my ($cpanm) = grep { -e $_ } ( which 'cpanm', '/usr/bin/cpanm', '/usr/local/bin/cpanm' );
-	die "Failed to install required libraries" if( ! -e $cpanm || $error );
-}
-
-my @errors = ( $error );
-foreach my $package (@missing) {
-	$error = system( "$cpanm --sudo $package" );
-	push @errors, $error if( $error );
-}
-
-if( @errors ) {
-	print map { "$_\n" } @errors;
-	exit( 1 );
-} else {
-	print "All dependencies satisfied.\n";
-	exit( 0 );
-}
-
+# ============================================================
 sub which ($) {
+# ============================================================
 	my $command = shift;
 	my $which = `which $command`;
 	chomp $which;
 	return $which;
+}
+
+# ============================================================
+sub bin ($) {
+# ============================================================
+	my $bin = shift;
+
+	my $found = first { -e $_ } ( which $bin, "/sbin/$bin", "/bin/$bin", "/usr/bin/$bin", "/usr/local/bin/$bin" );
+	return $found;
+}
+
+# ============================================================
+sub required_perl_modules {
+# ============================================================
+	my @modules = ();
+
+	push @modules,
+		grep { !/^(?:base|parent|warnings|strict|lib|Data::Dumper|FreeScore)/ } 
+		map { 
+			chomp;
+			s/^use\s*//;
+			s/\s\(?qw\([^)]+\)\)?//;
+			s/;$//;
+			s/\(\)$//;
+			exists $contains->{ $_ } ? $contains->{ $_ } : $_;
+		} `cd ../../ && grep -hir '^use' bin lib ../frontend/cgi-bin | sort | uniq`;
+
+	return @modules;
+}
+
+# ============================================================
+sub missing_perl_modules {
+# ============================================================
+	my @modules = @ARGV;
+	my @missing = grep {
+		my $test  = "$perl -M$_ -e 'exit();'";
+		my $error = system( "$test 2>/dev/null" );
+		$error;
+	} uniq sort @modules;
+	return @missing;
+}
+
+# ============================================================
+sub test_for_network {
+# ============================================================
+	my $ping = bin 'ping' || die "Can't test network";
+	my $error = system( "$ping -c 1 cpan.org" );
+	die "Not connected to the network" if( $error );
+}
+
+# ============================================================
+sub install_cpanm {
+# ============================================================
+# Use the curl utility to download cpanm
+	my $curl = bin 'curl';
+	if( ! $curl ) {
+		my $aptget = bin 'apt-get' || die "No package manager for this system";
+		my $error = system( "$aptget install -y curl" );
+		$curl = bin 'curl';
+		die "Failed to install module manager utility 'cpanm'" if( ! $curl || $error );
+	}
+	my $error = system( "curl -L https://cpanmin.us | $perl - --sudo App::cpanminus" );
+	my $cpanm = bin 'cpanm';
+	die "Failed to install module manager utility 'cpanm'" if( ! $cpanm || $error );
+
+	return $cpanm;
+}
+
+# ============================================================
+sub install_missing_modules {
+# ============================================================
+	my @missing = @ARGV;
+	my $cpanm = bin 'cpanm';
+
+	$cpanm = install_cpanm() if( ! $cpanm );
+
+	my @errors = ( $error );
+	foreach my $package (@missing) {
+		my $error = system( "$cpanm --sudo $package" );
+		push @errors, $error if( $error );
+	}
+
+	if( @errors ) {
+		print STDERR "Failed to install needed Perl modules:\n\n";
+		print STDERR map { "$_\n" } @errors;
+		exit( 1 );
+	}
+}
+
+# ============================================================
+sub install_history_system {
+# ============================================================
+	my $rcs = bin 'rcs';
+	return if $rcs;
+
+	my $aptget = bin 'apt-get' || die "No package manager for this system";
+	my $error  = system( "$aptget install -y rcs" );
+	$rcs = bin 'rcs';
+	die "Failed to install RCS history system" if( ! $rcs || $error );
+}
+
+# ============================================================
+sub configure_system_services {
+# ============================================================
+	system( "systemctl enable sshd" );
+	system( "systemctl start sshd" );
 }
