338cdf1d6c2609808cd8e6e83a20b2cda5c0e511
Mike Wong <mikewongtkd@gmail.com>
2019-02-26 09:57:50 -0800

    Improving dashboard

---
+++ trunk/frontend/html/staging/checkin/event-staging-overall-control.php
@@ -0,0 +1,19 @@
+<div class="overall-progress panel panel-primary">
+	<div class="panel-heading">
+		<h3 class="panel-title">Staging Progress</h3>
+	</div>
+	<div class="panel-body">
+		<div class="row">
+			<div class="col-xs-4">
+				<ul class="list-group">
+				</ul>
+			</div>
+			<div class="col-xs-8">
+				<div class="overall-label">Percent of Divisions Staged</div>
+				<div class="progress">
+					<div class="progress-bar" role="progress-bar"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
