9abed9441e6054e31029af3e5a83ba57f723236d
Mike Wong <mikewongtkd@gmail.com>
2019-04-01 05:26:28 -0700

    Improving staging system and implementing staging display

--- trunk/frontend/html/staging/display.php
+++ trunk/frontend/html/staging/display.php
@@ -1,28 +1,28 @@
 <?php
-	include "./include/php/version.php";
-	include "./include/php/config.php";
+	include "../include/php/version.php";
+	include "../include/php/config.php";
 ?>
 <html>
 	<head>
 		<title>Staging</title>
-		<link href="include/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-		<link href="include/bootstrap/add-ons/bootstrap-select.min.css" rel="stylesheet" />
-		<link href="include/css/freescore-light.css" rel="stylesheet" />
-		<link href="include/css/staging.css" rel="stylesheet" />
-		<link href="include/page-transitions/css/animations.css" rel="stylesheet" type="text/css" />
-		<link href="include/alertify/css/alertify.min.css" rel="stylesheet" />
-		<link href="include/alertify/css/themes/bootstrap.min.css" rel="stylesheet" />
-		<link href="include/fontawesome/css/font-awesome.min.css" rel="stylesheet" />
-		<script src="include/later/js/later.min.js"></script>
-		<script src="include/jquery/js/jquery.js"></script>
-		<script src="include/jquery/js/jquery.howler.min.js"></script>
-		<script src="include/jquery/js/jquery-dateformat.min.js"></script>
-		<script src="include/bootstrap/js/bootstrap.min.js"></script>
-		<script src="include/bootstrap/add-ons/bootstrap-select.min.js"></script>
-		<script src="include/bootstrap/add-ons/bootstrap-list-filter.min.js"></script>
-		<script src="include/bootstrap/add-ons/bootstrap-sortable.min.js"></script>
-		<script src="include/alertify/alertify.min.js"></script>
-		<script src="include/js/freescore.js"></script>
+		<link href="../include/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="../include/bootstrap/add-ons/bootstrap-select.min.css" rel="stylesheet" />
+		<link href="../include/css/freescore-dark.css" rel="stylesheet" />
+		<link href="../include/css/staging.css" rel="stylesheet" />
+		<link href="../include/page-transitions/css/animations.css" rel="stylesheet" type="text/css" />
+		<link href="../include/alertify/css/alertify.min.css" rel="stylesheet" />
+		<link href="../include/alertify/css/themes/bootstrap.min.css" rel="stylesheet" />
+		<link href="../include/fontawesome/css/font-awesome.min.css" rel="stylesheet" />
+		<script src="../include/later/js/later.min.js"></script>
+		<script src="../include/jquery/js/jquery.js"></script>
+		<script src="../include/jquery/js/jquery.howler.min.js"></script>
+		<script src="../include/jquery/js/jquery-dateformat.min.js"></script>
+		<script src="../include/bootstrap/js/bootstrap.min.js"></script>
+		<script src="../include/bootstrap/add-ons/bootstrap-select.min.js"></script>
+		<script src="../include/bootstrap/add-ons/bootstrap-list-filter.min.js"></script>
+		<script src="../include/bootstrap/add-ons/bootstrap-sortable.min.js"></script>
+		<script src="../include/alertify/alertify.min.js"></script>
+		<script src="../include/js/freescore.js"></script>
 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<style type="text/css">
@@ -36,22 +36,20 @@ body {
 var refresh = {};
 </script>
 		<div id="pt-main" class="pt-perspective">
-<?php include( 'staging/checkin.php' ); ?>
 		</div>
-<script src="include/page-transitions/js/pagetransitions.js"></script>
+<script src="../include/page-transitions/js/pagetransitions.js"></script>
 <script>
-alertify.set( 'notifier', 'position', 'top-right' );
 
 var sound = {
-	send      : new Howl({ urls: [ "./sounds/upload.mp3",   "./sounds/upload.ogg"   ]}),
-	confirmed : new Howl({ urls: [ "./sounds/received.mp3", "./sounds/received.ogg" ]}),
-	next      : new Howl({ urls: [ "./sounds/next.mp3",     "./sounds/next.ogg"     ]}),
-	previous  : new Howl({ urls: [ "./sounds/prev.mp3",     "./sounds/prev.ogg"     ]}),
+	send      : new Howl({ urls: [ "../sounds/upload.mp3",   "../sounds/upload.ogg"   ]}),
+	confirmed : new Howl({ urls: [ "../sounds/received.mp3", "../sounds/received.ogg" ]}),
+	next      : new Howl({ urls: [ "../sounds/next.mp3",     "../sounds/next.ogg"     ]}),
 };
 
 var host       = '<?= $host ?>';
 var tournament = <?= $tournament ?>;
 var html       = FreeScore.html;
+var schedule   = {};
 
 var page = {
 	num : 1,
@@ -106,6 +104,10 @@ server.worldclass.onmessage = ( response ) => {
 	console.log( 'Worldclass', update );
 };
  */
+
+var timer = setInterval(() => {
+	var time = $.format.date( new Date(), "yyyy-MM-dd h:mm a" );
+}, 5000 );
 </script>
 	</body>
 </html>
