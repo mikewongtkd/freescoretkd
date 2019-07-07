b98724f387f2a0b2e476eda3ab254fc4e5a475ea
Mike Wong <mikewongtkd@gmail.com>
2019-07-07 02:40:03 -0700

    Added announcer feature

--- trunk/frontend/html/staging.php
+++ trunk/frontend/html/staging.php
@@ -35,12 +35,35 @@ body {
 <script>
 var refresh = {};
 </script>
+		<button class="btn btn-primary" id="announcer">Enable Announcer</button>
 		<div id="pt-main" class="pt-perspective">
 <?php include( 'staging/checkin.php' ); ?>
 		</div>
 <script src="include/page-transitions/js/pagetransitions.js"></script>
 <script>
 alertify.set( 'notifier', 'position', 'top-right' );
+
+var announcer = { messages: []};
+announcer.message = ( text ) => {
+	announcer.messages.push( text );
+	$( '#announcer' ).removeClass( 'disabled' ).html( `Announcer has ${announcer.messages.length} messages` );
+};
+announcer.speak = () => {
+	if( announcer.messages.length == 0 ) { return; }
+	let text = announcer.messages.shift();
+	if( typeof text === 'object' ) { text = `${text.first_name} ${text.last_name}`; alertify.notify( `<b>Announcer</b><br>${text}` ); text = text.toLowerCase(); } else { alertify.notify( `<b>Announcer</b><br>${text}` ); }
+	let message = new SpeechSynthesisUtterance( text );
+	message.voice = speechSynthesis.getVoices().filter( voice => voice.name == 'Google US English' ).shift();
+	window.speechSynthesis.speak( message )
+	message.onend = ( e ) => { 
+		let n = announcer.messages.length;
+		if( n == 0 ) {
+			$( '#announcer' ).addClass( 'disabled' ).html( 'Announcer Muted' );
+		} else {
+			$( '#announcer' ).html( `Announcer has ${n} messages` );
+		}
+		setTimeout(() => { announcer.speak(); }, 800 ); }; // Wait 1 second and say the next item in the queue
+};
 
 var sound = {
 	send      : new Howl({ urls: [ "./sounds/upload.mp3",   "./sounds/upload.ogg"   ]}),
@@ -63,6 +86,23 @@ var page = {
 		}
 	}
 };
+
+$(() => {
+	announcer.message( 'Hello' );
+	announcer.message( 'First call for Obstacle Course Division OC01A' );
+	registration.events[ 'Obstacle Course' ][ 'oc01a' ].athletes.forEach(( id ) => { announcer.message( registration.athletes[ id ]); });
+	announcer.message( 'This is your first call for Obstacle Course Division OC01A. You have 30 minutes to report to the staging area.' );
+	announcer.message( 'Repeating first call for Obstacle Course Division OC01A' );
+	registration.events[ 'Obstacle Course' ][ 'oc01a' ].athletes.forEach(( id ) => { announcer.message( registration.athletes[ id ]); });
+	announcer.message( 'Please report to the staging area for Obstacle Course Division OC01A' );
+	announcer.message( 'Again, first call for Obstacle Course Division OC01A' );
+	registration.events[ 'Obstacle Course' ][ 'oc01a' ].athletes.forEach(( id ) => { announcer.message( registration.athletes[ id ]); });
+	announcer.message( 'This is your first call for Obstacle Course Division OC01A. You have 30 minutes.' );
+});
+
+$( '#announcer' ).off( 'click' ).click(( ev ) => {
+	announcer.speak();
+});
 
 // ===== SERVER COMMUNICATION
 var server = {
