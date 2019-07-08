2dbab6d995ae3e380210ec33164be679fa6f03ff
Mike Wong <mikewongtkd@gmail.com>
2019-07-07 23:58:42 -0700

    Updating for new checkin by division (rather than athlete), since athletes can wander off between divisions.

--- trunk/frontend/html/staging.php
+++ trunk/frontend/html/staging.php
@@ -51,7 +51,7 @@ announcer.message = ( text ) => {
 announcer.speak = () => {
 	if( announcer.messages.length == 0 ) { return; }
 	let text = announcer.messages.shift();
-	if( typeof text === 'object' ) { text = `${text.first_name} ${text.last_name}`; alertify.notify( `<b>Announcer</b><br>${text}` ); text = text.toLowerCase(); } else { alertify.notify( `<b>Announcer</b><br>${text}` ); }
+	if( typeof text === 'object' ) { alertify.notify( `<b>Announcer</b><br>${text.name}` ); text = text.name.toLowerCase(); } else { alertify.notify( `<b>Announcer</b><br>${text}` ); }
 	let message = new SpeechSynthesisUtterance( text );
 	message.voice = speechSynthesis.getVoices().filter( voice => voice.name == announcer.voice.english ).shift();
 	window.speechSynthesis.speak( message )
@@ -63,6 +63,35 @@ announcer.speak = () => {
 			$( '#announcer' ).html( `Announcer has ${n} messages` );
 		}
 		setTimeout(() => { announcer.speak(); }, 800 ); }; // Wait 1 second and say the next item in the queue
+};
+announcer.call = ( division, call ) => {
+	if( division.athletes.every( x => x.checkin )) { return; } // Skip the call if every athlete is checked in
+	let map   = { '1': { ordinal: 'First', time: 30 }, '2': { ordinal: 'Second', time: 15 }, '3': { ordinal: 'Third', time: 5 }}; call = map[ call ];
+	let div   = `${division.event} ${division.description} Division ${division.id}`;
+	let intro = [ 
+		`${call.ordinal} call for ${div}`, 
+		`Attention athletes, ${call.ordinal} call for ${div}`, 
+		`This is your ${call.ordinal} call for ${div}`,
+	];
+	let repeat = [
+		`Attention athletes, ${call.ordinal} call for ${div}`, 
+		`This is your ${call.ordinal} call for ${div}`,
+		`Repeating ${call.ordinal} call for ${div}`,
+		`Again, ${call.ordinal} call for ${div}`,
+	]
+	let outro = [
+		`Please report to the staging area. You have ${call.time} minutes.`,
+		`You have ${call.time} minutes to report to the staging area.`,
+	];
+	let pick = ( choices ) => { let i = Math.floor( Math.random() * choices.length ); return choices[ i ]; }
+
+	announcer.message( pick( intro ));
+	division.athletes.forEach(( athlete ) => { if( athlete.checkin ) { return; } announcer.message( registration.athletes[ athlete.id ]); });
+	announcer.message( pick( repeat ));
+	division.athletes.forEach(( athlete ) => { if( athlete.checkin ) { return; } announcer.message( registration.athletes[ athlete.id ]); });
+	announcer.message( pick( repeat ));
+	division.athletes.forEach(( athlete ) => { if( athlete.checkin ) { return; } announcer.message( registration.athletes[ athlete.id ]); });
+	announcer.message( pick( outro ));
 };
 
 var sound = {
@@ -89,15 +118,6 @@ var page = {
 
 $(() => {
 	announcer.message( 'Hello' );
-	announcer.message( 'First call for Obstacle Course Division OC01A' );
-	registration.events[ 'Obstacle Course' ][ 'oc01a' ].athletes.forEach(( id ) => { announcer.message( registration.athletes[ id ]); });
-	announcer.message( 'This is your first call for Obstacle Course Division OC01A. You have 30 minutes to report to the staging area.' );
-	announcer.message( 'Repeating first call for Obstacle Course Division OC01A' );
-	registration.events[ 'Obstacle Course' ][ 'oc01a' ].athletes.forEach(( id ) => { announcer.message( registration.athletes[ id ]); });
-	announcer.message( 'Please report to the staging area for Obstacle Course Division OC01A' );
-	announcer.message( 'Again, first call for Obstacle Course Division OC01A' );
-	registration.events[ 'Obstacle Course' ][ 'oc01a' ].athletes.forEach(( id ) => { announcer.message( registration.athletes[ id ]); });
-	announcer.message( 'This is your first call for Obstacle Course Division OC01A. You have 30 minutes.' );
 });
 
 $( '#announcer' ).off( 'click' ).click(( ev ) => {
