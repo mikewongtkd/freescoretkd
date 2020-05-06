60ea97ae4c0c4d5a85498ff321d48f76386b1330
Mike Wong <mikewongtkd@gmail.com>
2020-05-06 13:41:36 -0700

    Moving config information to JSON file.

--- trunk/frontend/html/include/php/config.php
+++ trunk/frontend/html/include/php/config.php
@@ -5,17 +5,41 @@
 		return intval( $n );
 	};
 
-	$host       = "freescore.net";
-	$tournament = [ 
-		"name" => "FreeScore",
-		"db"   => "test", 
-	];
-	$rings = [];
-	$rings[ 'grassroots' ] = preg_grep( '/ring|staging/', scandir( '/usr/local/freescore/data/' . $tournament[ 'db' ] . '/forms-grassroots' ));
-	$rings[ 'worldclass' ] = preg_grep( '/ring|staging/', scandir( '/usr/local/freescore/data/' . $tournament[ 'db' ] . '/forms-worldclass' ));
-	$rings[ 'freestyle' ]  = preg_grep( '/ring|staging/', scandir( '/usr/local/freescore/data/' . $tournament[ 'db' ] . '/forms-freestyle'  ));
-	$rings = array_values( array_filter( array_map( 'get_ring_number', array_unique( array_merge( $rings[ 'grassroots' ], $rings[ 'worldclass' ], $rings[ 'freestyle' ] )))));
-	asort( $rings );
-	$tournament[ 'rings' ] = $rings;
-	$tournament = json_encode( $tournament );
+	function read_config() {
+		$string = file_get_contents( 'config.json' );
+		$config = json_decode( $string, true );
+		return $config;
+	}
+
+	function init_event( $event ) {
+		mkdir( $event, 0777, true );
+		mkdir( "$event/ring01", 0777, true );
+		mkdir( "$event/staging", 0777, true );
+	}
+
+	function read_rings( $tournament ) {
+		// Initialize rings
+		$grassroots = '/usr/local/freescore/data/' . $tournament[ 'db' ] . '/forms-grassroots';
+		$worldclass = '/usr/local/freescore/data/' . $tournament[ 'db' ] . '/forms-worldclass';
+		$freestyle  = '/usr/local/freescore/data/' . $tournament[ 'db' ] . '/forms-freestyle';
+
+		if( ! file_exists( $grassroots )) { init_event( $grassroots ); }
+		if( ! file_exists( $worldclass )) { init_event( $worldclass ); }
+		if( ! file_exists( $freestyle  )) { init_event( $freestyle  ); }
+
+		$rings = [];
+		$rings[ 'grassroots' ] = preg_grep( '/ring|staging/', scandir( $grassroots ));
+		$rings[ 'worldclass' ] = preg_grep( '/ring|staging/', scandir( $worldclass ));
+		$rings[ 'freestyle' ]  = preg_grep( '/ring|staging/', scandir( $freestyle  ));
+		$rings = array_values( array_filter( array_map( 'get_ring_number', array_unique( array_merge( $rings[ 'grassroots' ], $rings[ 'worldclass' ], $rings[ 'freestyle' ] )))));
+		asort( $rings );
+		$tournament[ 'rings' ] = $rings;
+		$tournament = json_encode( $tournament );
+	}
+
+	$config     = read_config();
+	$host       = $config[ 'host' ];
+	$tournament = $config[ 'tournament' ];
+	read_rings( $tournament );
+
 ?>
