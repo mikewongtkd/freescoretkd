e328cca8cfb930073d0554b41e2d45e4dca25303
Mike Wong <mikewong@sfsu.edu>
2021-03-26 15:29:43 -0700

    Speed kicking ordering bug fixed

--- trunk/backend/lib/FreeScore/Kicking/Speed/Division.pm
+++ trunk/backend/lib/FreeScore/Kicking/Speed/Division.pm
@@ -2,7 +2,7 @@ package FreeScore::Kicking::Speed::Division;
 use FreeScore;
 use FreeScore::Forms::Division;
 use JSON::XS;
-use List::Util qw( min reduce shuffle );
+use List::Util qw( any min reduce shuffle );
 use List::MoreUtils qw( all first_index last_index minmax part );
 use Data::Structure::Util qw( unbless );
 use File::Slurp qw( read_file );
@@ -187,11 +187,12 @@ sub read {
 	my $aids = []; # Athlete IDs
 	foreach my $earliest (qw( prelim semfin finals ), @matches ) {
 		next unless exists $order->{ $earliest };
+		$self->{ order }{ $earliest } = [];
 		while( my $name = shift @{ $order->{ $earliest }}) {
 			my $athlete = $athletes->{ $name };
-			push @$aids, { name => $name, info => $athlete->{ info }, scores => $athlete->{ scores }, pool => $athlete->{ pool }};
+			push @$aids, { name => $name, id => int( @$aids ), info => $athlete->{ info }, scores => $athlete->{ scores }, pool => $athlete->{ pool }};
 			delete $athletes->{ $name };
-			push @{$self->{ order }{ $earliest }}, $#$aids;
+			push @{$self->{ order }{ $earliest }}, $#$aids unless any { $_ == $#$aids } @{ $self->{ order }{ $earliest }} ;
 		}
 		last unless int( keys %$athletes ) > 0;
 	}
