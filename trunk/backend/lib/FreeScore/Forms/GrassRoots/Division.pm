432c55a1bd2acb17c722a80b3ea0ad42e4420535
Andrew Pang <pang.134@osu.edu>
2021-04-03 17:38:14 -0400

    Change round names from fin to finals and tb to tiebreaker

--- trunk/backend/lib/FreeScore/Forms/GrassRoots/Division.pm
+++ trunk/backend/lib/FreeScore/Forms/GrassRoots/Division.pm
@@ -15,7 +15,7 @@ sub read {
 	my $index = 0;
 	$self->{ judges } = 3; # Default value
 	$self->{ places } = [ { place => 1, medals => 1 }, { place => 2, medals => 1 }, { place => 3, medals => 2 } ];
-	$self->{ round }  = 'fin';
+	$self->{ round }  = 'finals';
 
 	my $json = new JSON::XS();
 
@@ -163,11 +163,11 @@ sub calculate_placements {
 		# Complete, unresolved
 		if( @$tied ) { 
 			$self->{ tied } = $tied;       
-			if( $self->{ round } eq 'fin' ) {
+			if( $self->{ round } eq 'finals' ) {
 				my $first = $tied->[ 0 ]{ tied }[ 0 ];
 				$self->{ current } = $first;
 			}
-			$self->{ round } = 'tb';
+			$self->{ round } = 'tiebreaker';
 
 		# Complete, resolved
 		} else { 
