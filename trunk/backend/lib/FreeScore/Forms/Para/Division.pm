63ff322b5a697ef9dee8b13a6bcb6c56f2441d0d
Andrew Pang <pang.134@osu.edu>
2021-04-28 14:15:12 -0400

    Bless the value

--- trunk/backend/lib/FreeScore/Forms/Para/Division.pm
+++ trunk/backend/lib/FreeScore/Forms/Para/Division.pm
@@ -542,14 +542,13 @@ sub record_athlete_info {
 	my $value   = shift;
 
 	my $athlete = $self->{ athletes }[ $self->{ current } ];
-	my $round   = $self->{ round };
 	my $json    = new JSON::XS();
 
 	$value = $json->canonical->encode( $value ) if( ref( $value ));
 
 	$athlete->{ info }{ $key } = $value;
 
-	return $athlete->{ info }->record_athlete_info( $key, $value );
+	return $athlete->{ info }
 }
 
 # ============================================================
