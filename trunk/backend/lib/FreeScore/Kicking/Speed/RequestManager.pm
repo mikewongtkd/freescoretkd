700e1e2cec57a8d6731312824aade01cbbdd3b4c
Andrew Pang <pang.134@osu.edu>
2021-11-03 14:35:03 -0400

    Request manager was missing the round before

--- trunk/backend/lib/FreeScore/Kicking/Speed/RequestManager.pm
+++ trunk/backend/lib/FreeScore/Kicking/Speed/RequestManager.pm
@@ -154,11 +154,11 @@ sub handle_division_award_penalty {
 	my $client   = $self->{ _client };
 	my $division = $progress->current();
 
+	my $round    = $division->{ round };
 	my $athlete  = $division->{ athletes }[ $request->{ athlete_id } ];
 	my $have     = $athlete->{ penalty }{ $round };
 	my $add      = clone( $request->{ penalty });
-
-	foreach my $penalty (keys %$have) { delete $add->{ $penalty } if exists $add->{ $penalty }; }
+	
 	if( keys %$add ) {
 		print STDERR "Award penalty: " . join( ' ', sort keys %$add ) . " to $athlete->{ name }.\n" if $DEBUG;
 	} else {
