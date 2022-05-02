5512fc438930216c9b94b5ff43454d985b952ffe
Mike Wong <mikewong@sfsu.edu>
2022-05-02 13:41:26 -0700

    Updating Docker build to include CGI

--- docker/bin/install.sh
+++ docker/bin/install.sh
@@ -8,6 +8,7 @@ apt-get install -y sudo vim cpanminus
 
 cpanm install \
 	Carp \
+	CGI \
 	Clone \
 	Data::Structure::Util \
 	Date::Manip \
@@ -30,4 +31,5 @@ cpanm install \
 	Time::Piece \
 	Try::Tiny
 
-a2enmod proxy proxy_http proxy_wstunnel
+a2enmod cgi proxy proxy_http proxy_wstunnel
+perl -pi -e 's|/usr/lib/cgi-bin/|/var/www/cgi-bin/|' /etc/apache2/conf-enabled/serve-cgi-bin.conf
