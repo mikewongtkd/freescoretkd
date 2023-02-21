21f8b0b01161fcf720b064bf740a226648a931db
Mike Wong <mikewong@sfsu.edu>
2023-02-20 20:32:05 -0800

    Fixed typo

--- trunk/frontend/html/include/php/config.php
+++ trunk/frontend/html/include/php/config.php
@@ -22,7 +22,7 @@
 		$http = 'http://';
 		$host = $config[ 'host' ];
 		$port = '';
-		if( ! array_key_exists( 'protocol', $config )) { $http = 'http://' } # Default
+		if( ! array_key_exists( 'protocol', $config )) { $http = 'http://'; } # Default
 		if( ! array_key_exists( 'port', $config ) || $config[ 'port' ] == 80 ) { $port = ''; } else { $port = ":{$port}"; }
 
 		$url = "{$http}{$host}{$port}";
