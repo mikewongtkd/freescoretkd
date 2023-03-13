e2e37dcaa3169103888536a815eada68abe86a3e
Mike Wong <mikewong@sfsu.edu>
2023-03-13 00:57:05 -0700

    Adding Session class

--- trunk/frontend/html/include/php/session/authenticate.php
+++ trunk/frontend/html/include/php/session/authenticate.php
@@ -2,19 +2,5 @@
 	include_once( __DIR__ . "/../config.php" );
 	include_once( __DIR__ . "/../../../session.php" );
 
-	verify_authentication( $config );
-
-	function verify_authentication( $config ) {
-		// If the app is configured without passwords, then no authentication is necessary
-		if( is_null( $config->password())) { return; }
-
-		// Otherwise redirect the user if they are not authorized
-		$host = $config->host();
-		if( ! array_key_exists( 'is_auth', $_SESSION ) || ! $_SESSION[ 'is_auth' ]) {
-			$referrer = urlencode( base64_encode( $_SERVER[ 'HTTP_REFERER' ]));
-			$message  = urlencode( base64_encode( 'User is unauthorized' ));
-			header( "Location: {$host}/login.php?referrer={$referrer}&{$message}" );
-			exit();
-		}
-	}
+	Session::authenticate();
 ?>
--- trunk/frontend/html/session.php
+++ trunk/frontend/html/session.php
@@ -2,4 +2,62 @@
 	session_name( 'freescore-session' );
 	session_save_path( '/usr/local/freescore/sessions' );
 	session_start();
+
+	class Session {
+		static function authenticate( $password = null ) {
+			$ring     = null;
+			$referrer = null;
+
+			// If already authorized, do nothing
+			if( isset( $_SESSION[ 'is_auth' ]) && $_SESSION[ 'is_auth' ]) { return; }
+
+			// Set referrer
+			if( isset( $_GET[ 'referrer' ])) { $referrer = $_GET[ 'referrer' ]; }
+			if( ! isset( $referrer )) { $referrer = Session::urlencode( $_SERVER[ 'HTTP_REFERER' ]); }
+
+			// Get password
+			if( ! isset( $password ) && ! isset( $_POST[ 'password' ])) {
+				Session::error( 'Please provide a password', $referrer );
+			}
+			$password = $_POST[ 'password' ];
+
+			// Get ring
+			if     ( isset( $_GET[ 'ring' ]))  { $ring = $_GET[ 'ring' ];  $ring = $ring == 'staging' ? $ring : sprintf( 'ring%02d', $ring ); }
+			else if( isset( $_POST[ 'ring' ])) { $ring = $_POST[ 'ring' ]; $ring = $ring == 'staging' ? $ring : sprintf( 'ring%02d', $ring ); }
+
+			$correct = $config->password( $ring );
+
+			if( $password != $correct ) {
+				Session::error( 'Password does not match our records' );
+			}
+
+			$_SESSION[ 'is_auth' ] = 1;
+			Session::redirect( $referrer );
+		}
+
+		static function redirect( $referrer = null ) {
+
+			if( isset( $message )) {
+				header( "Location: {$referrer}" );
+				exit();
+			}
+		}
+
+		static function error( $message = null ) {
+			global $config;
+			$host = $config->host();
+			if( isset( $message )) {
+				header( "Location: ${host}/login.php?referrer={$referrer}&message={$message}" );
+				exit();
+			}
+		}
+
+		static function urlencode( $string ) {
+			return urlencode( base64_encode( $string ));
+		}
+
+		static function urldecode( $string ) {
+			return base64_decode( urldecode( $string ));
+		}
+	}
 ?>
