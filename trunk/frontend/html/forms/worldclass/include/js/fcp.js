162ac97cf924c0d6b13199608780e270db22d661
Mike Wong <mikewong@sfsu.edu>
2025-04-05 11:04:46 -0700

    Adding parameter validation prior to sending for FCP

--- trunk/frontend/html/forms/worldclass/include/js/fcp.js
+++ trunk/frontend/html/forms/worldclass/include/js/fcp.js
@@ -21,8 +21,14 @@ FreeScore.CommsProtocol.WorldClass = class FSCommsProtocolPWorldClass extends Fr
 		this._request.div.rec = {
 			award: { 
 				minScore: () => { this.send( 'division', 'award min score' ); },
-				penalty: ( penalties ) => { this.send( 'division', 'award min score', { penalties }); },
-				punitive: ( decision ) => { this.send( 'division', 'award min score', { decision }); },
+				penalty: ( penalties ) => { 
+					if( ! this.validate.penalties( penalties )) { return; }
+					this.send( 'division', 'award penalty', { penalties }); 
+				},
+				punitive: ( decision ) => { 
+					if( ! this.validate.decision( decision )) { return; }
+					this.send( 'division', 'award punitive', { decision }); 
+				}
 			},
 			clearJudgeScore: ( judge ) => { this.send( 'division', 'clear judge score', { judge }); },
 			display: () => { this.send( 'division', 'display' ); },
@@ -32,8 +38,19 @@ FreeScore.CommsProtocol.WorldClass = class FSCommsProtocolPWorldClass extends Fr
 		};
 
 		this.validate.penalties = penalties => {
-
+			const valid = [ 'bounds', 'restart', 'timelimit', 'misconduct' ];
+			if( typeof penalties !== 'object' ) {
+				console.log( 'FCP Validate Penalties: penalty is not an object', penalties );
+				return false;
+			}
+			let ok = Object.keys( penalties ).reduce(( acc, cur ) => acc && valid.includes( cur ), true );
+			return ok;
 		};
+
+		this.validate.decisions = decision => {
+			const valid = [ 'disqualify', 'withdraw' ];
+			return valid.includes( decision );
+		}
 
 		this._request.div.drawSBSPoomsae = ( option ) => { 
 			if( typeof option === 'string' ) {
