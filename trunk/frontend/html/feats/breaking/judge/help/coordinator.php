a5deb4f8c96d523bf13f743014456273737369ec
Mike Wong <mikewongtkd@gmail.com>
2026-04-23 00:41:43 -0700

    Need to confirm autopilot for breaking. Also consider settings and reconnect buttons, as well as auto-reconnect on send.

--- trunk/frontend/html/feats/breaking/judge/help/coordinator.php
+++ trunk/frontend/html/feats/breaking/judge/help/coordinator.php
@@ -10,5 +10,11 @@
 	<li>Competitor and board holders clean up</li>
 	<li><i>Char-yeot</i></li>
 	<li><i>Kyeong-rye</i></li>
-	<li>Scorekeeper stops timer</li>
+	<li>Scorekeeper stops timer
+	<ul>
+		<li>If the timer shows more than 3 minutes elapsed:</li>
+		<li>The system shall inform the Scorekeeper how many deductions.</li>
+		<li>The Scorekeeper must tell the Referee.</li>
+		<li>The Referee must announce <i>Overtime!</i> and record the deduction(s)</li>
+	</ul></li>
 </ul>
